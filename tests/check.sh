d74a36c2e5087e44d3294df06427f63b60456948
arno <arno>
2000-06-05 21:24:21 +0000

    * configure.in tests/Makefile.am: Updates for the new 	directories.
    	* tests/check.sh tests/filters/.cvsignore
    	tests/filters/Makefile.am tests/filters/lowpass.cir
    	tests/filters/lowpass.out tests/resistance/.cvsignore
    	tests/resistance/Makefile.am tests/resistance/res_array.cir
    	tests/resistance/res_array.out
    	tests/resistance/res_partition.cir
    	tests/resistance/res_partition.out
    	tests/resistance/res_simple.cir
    	tests/resistance/res_simple.out: New files.
    
    	* tests/config.sh tests/diffpair.sh tests/fourbitadder.sh
    	tests/resistor.cir tests/resistor.out tests/resistor.sh
    	tests/respart.cir tests/respart.out tests/respart.sh
    	tests/resistance/test1.cir: No longer needed, removed.

---
+++ tests/check.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+NGSPICE=$1
+TEST=$2
+
+DIFFPIPE="Analysis|CPU|memory|Date|Note"
+
+testname=$(basename $TEST .cir)
+testdir=$(dirname $TEST)
+$NGSPICE < $testdir/$testname.cir 2>&1 | egrep -v $DIFFPIPE > $testname.test
+if diff -u $testdir/$testname.out $testname.test; then
+    rm $testname.test
+    exit 0
+fi
+exit 1
--- tests/resistor.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-
-. $srcdir/config.sh
-spicetest resistor
