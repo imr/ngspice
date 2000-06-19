6f81cb9446e25ce65435e87f56a3f30400f419fc
arno <arno>
2000-06-19 19:03:54 +0000

    * src/analysis/cktask.c: moved to src/devices.
    	* src/analysis/cktbindn.c: moved, renamed to
    	src/devices/cktbindnode.c.
    
    	* src/analysis/cktfdev.c: moved, renamed to
    	src/devices/cktfinddev.c.
    
    	* TODO: Updates
    
    	* src/analysis/Makefile.am src/devices/Makefile.am: Updates.

--- src/spicelib/devices/cktask.c
+++ src/spicelib/devices/cktask.c
@@ -3,30 +3,30 @@ Copyright 1990 Regents of the University of California.  All rights reserved.
 Author: 1985 Thomas L. Quarles
 **********/
 
-    /* CKTask
-     *  Ask questions about a specified device.
-     */
+/* CKTask
+ *
+ * Ask questions about a specified device.  */
 
-#include "ngspice.h"
-#include <stdio.h>
-#include "cktdefs.h"
-#include "ifsim.h"
-#include "devdefs.h"
-#include "sperror.h"
+#include <config.h>
+#include <devdefs.h>
+#include <sperror.h>
 
+#include "dev.h"
 
-extern SPICEdev *DEVices[];
 
 int
 CKTask(void *ckt, void *fast, int which, IFvalue *value, IFvalue *selector)
 {
-    int type = ((GENinstance *)fast)->GENmodPtr->GENmodType;
+    GENinstance *instance = (GENinstance *) fast;
+    int type = instance->GENmodPtr->GENmodType;
     int error;
 #ifdef PARALLEL_ARCH
     long msgtype, length;
-    long from = ((GENinstance *)fast)->GENowner;
+    long from = instance->GENowner;
 #endif /* PARALLEL_ARCH */
+    SPICEdev **DEVices;
 
+    DEVices = devices();
     if((*DEVices[type]).DEVask) {
         error = DEVices[type]->DEVask((CKTcircuit *)ckt,
                 (GENinstance *)fast,which,value,selector);
