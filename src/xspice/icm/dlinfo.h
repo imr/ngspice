ca37aa0858855a41c6f55a5bc9f4766e2f0f738f
pnenzi <pnenzi>
2003-08-05 15:24:27 +0000

    Removed dlinfo.h

--- src/xspice/icm/dlinfo.h
+++
@@ -1,9 +0,0 @@
-//////////////////////////////////////////////////////////////////////////////
-// Modify these to include general information about the library
-//////////////////////////////////////////////////////////////////////////////
-char inf_Title[]="icm_poly.cm";
-char inf_Version[]="0.1";
-char inf_Description[]="Code model implementation of SPICE2 poly.";
-char inf_Author[]="Hacked together by SDB, June 2003.";
-char inf_Copyright[]="Released under GPL";
-
