003a286109b2c7ef74a5c80d9a9a0568f70f9f3e
pnenzi <pnenzi>
2003-08-11 20:59:48 +0000

    Update after CIDER inclusion.

--- acconfig.h
+++ acconfig.h
@@ -17,6 +17,15 @@
 /* Define if we want NOBYPASS */ 
 #undef NOBYPASS
 
+/* Define if we want to bypass cbd/cbs calculation for non varying vbs/vbd */
+ #undef CAPBYPASS 
+ 
+/* Define if we want to bypass cbd/cbs calculation if Czero is zero */
+#undef CAPZEROBYPASS 
+ 
+/* Experimental code never implemented to damp Newton iterations */
+#undef NODELIMITING
+
 /* Define if we want predictor algorithm */ 
 #undef PREDICTOR
 
@@ -32,15 +41,30 @@
 /* Undefine HAVE_EKV since it is not included in the standard distribution */
 #undef HAVE_EKV
 
-/* Undefine HAVE_GNUREADLINE */   
+/* Define if we have GNU readline */   
 #undef HAVE_GNUREADLINE
 
 /* We do not want spurios debug info into non-developer code */
 #undef FTEDEBUG
 
+/* Do not trigger unwanted traps by default */
+#undef NEWTRUNC
+  
+/* Define if we wanto debug sensititvity analysis */
+#undef SENSDEBUG
+
+/* Define i we want stepdebug */
+#undef STEPDEBUG
+  
+/* Define to use always exp/log for bulk diode calculations in mosfet */
+#undef NOSQRT
+
 /*The xspice enhancements*/
 #undef XSPICE
 
+/* The CIDER enhancements */
+#undef CIDER
+ 
 /* Spice cluster support */
 #undef CLUSTER
 
