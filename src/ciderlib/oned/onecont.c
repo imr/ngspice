3765922b2357844d322f732ac99995006ffc5de4
pnenzi <pnenzi>
2003-08-13 23:23:06 +0000

    Cider inclusion fix.

--- src/ciderlib/oned/onecont.c
+++ src/ciderlib/oned/onecont.c
@@ -8,7 +8,7 @@ Author:	1987 Kartikeya Mayaram, U. C. Berkeley CAD Group
 #include "numenum.h"
 #include "onemesh.h"
 #include "onedev.h"
-#include "spMatrix.h"
+#include "spmatrix.h"
 #include "macros.h"
 #include "onedext.h"
 #include "oneddefs.h"
