488e1cf2b315a28dacf6e09be1c1c435dcb859fd
pnenzi <pnenzi>
2003-09-25 06:53:41 +0000

    Added a couple of constants: epsilon0 and epsilonSiO2.

--- src/include/const.h
+++ src/include/const.h
@@ -10,6 +10,8 @@ Author: 1985 Thomas L. Quarles
 #define CHARGE (1.6021918e-19)
 #define CONSTCtoK (273.15)
 #define CONSTboltz (1.3806226e-23)
+#define CONSTepsZero (8.854214871e-12)   /* epsilon zero F/m */
+#define CONSTepsSiO2 (3.4531479969e-11)  /* epsilon SiO2 F/m */
 #define REFTEMP 300.15 /* 27 degrees C */
 
     extern double CONSTroot2;
--- src/include/defines.h
+++ src/include/defines.h
@@ -62,6 +62,9 @@
 #define CHARGE (1.6021918e-19)
 #define CONSTCtoK (273.15)
 #define CONSTboltz (1.3806226e-23)
+#define CONSTepsZero (8.854214871e-12)   /* epsilon zero F/m */
+#define CONSTepsSiO2 (3.4531479969e-11)  /* epsilon SiO2 F/m */
+
 #define REFTEMP 300.15 /* 27 degrees C */
 
 
