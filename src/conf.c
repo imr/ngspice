966dd078456932aa861ef8c2d5e6be9f8bced5c9
sjborley <sjborley>
2005-05-20 11:26:32 +0000

    now uses values set by AC_INIT in configure.in

--- src/conf.c
+++ src/conf.c
@@ -1,16 +1,20 @@
 /* Common configuration file for ng-spice and nutmeg */
 
 #include <config.h>
-
 #include "conf.h"
 
-char	Spice_Version[] = VERSION;
-char	Spice_Notice[] = "Please submit bug-reports to: ngspice-bugs@lists.sourceforge.net";
+/*  The GNU configure system should set PACKAGE_BUGREPORT
+	use this if it does, otherwise fall back to a hardcoded address */
+#ifdef PACKAGE_BUGREPORT
+#define BUG_ADDRESS		PACKAGE_BUGREPORT
+#else
+#define BUG_ADDRESS		"ngspice-bugs@lists.sourceforge.net"
+#endif
+
+char	Spice_Version[] = PACKAGE_VERSION;
+char	Spice_Notice[] = "Please submit bug-reports to: " BUG_ADDRESS;
 char	Spice_Build_Date[] = NGSPICEBUILDDATE;
-/*
-char	*Spice_Exec_Dir	= "/spice_win/bin";
-char	*Spice_Lib_Dir	= "/spice_win/lib";
-*/
+
 char	*Spice_Exec_Dir	= NGSPICEBINDIR;
 char	*Spice_Lib_Dir	= NGSPICEDATADIR;
 
@@ -22,8 +26,6 @@ char	*Def_Editor	= "vi";
 int	AsciiRawFile	= 0;
 #endif
 
-
-char	*Bug_Addr	= "ngspice-bugs@lists.sourceforge.net";
+char	*Bug_Addr	= BUG_ADDRESS;
 char	*Spice_Host	= "";
 char	*Spiced_Log	= "";
-
