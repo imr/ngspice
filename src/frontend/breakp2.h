b7701cd0e9329829c04137fdbdbf8dd0e1ef4e8b
sjborley <sjborley>
2005-05-29 01:01:40 +0000

    Added access to dbs and debugnumber

--- src/frontend/breakp2.h
+++ src/frontend/breakp2.h
@@ -1,6 +1,7 @@
 /*************
  * Header file for breakp2.c
  * 1999 E. Rouat
+ * $Id$
  ************/
 
 #ifndef BREAKP2_H_INCLUDED
@@ -12,5 +13,7 @@ void com_save2(wordlist *wl, char *name);
 void settrace(wordlist *wl, int what, char *name);
 int ft_getSaves(struct save_info **savesp);
 
+extern struct dbcomm *dbs;
+extern int debugnumber;
 
 #endif
