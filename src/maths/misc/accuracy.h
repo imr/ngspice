c8babc9450681cf208c79b14ad742088dca08591
sjborley <sjborley>
2005-05-31 16:37:22 +0000

    Added prototype for evalAccLimits()

--- src/maths/misc/accuracy.h
+++ src/maths/misc/accuracy.h
@@ -1,14 +1,21 @@
 /**********
 Copyright 1991 Regents of the University of California. All rights reserved.
 Authors: 1987 Karti Mayaram, 1991 David Gates
+$Id$
 **********/
+
+#ifndef ACCURACY_H
+#define ACCURACY_H
+
 /*
  * Definitions of Globals for Machine Accuracy Limits
  */
-#ifndef ACC_H
-#define ACC_H
+
 extern double BMin;          /* lower limit for B(x) */
 extern double BMax;          /* upper limit for B(x) */
 extern double ExpLim;        /* limit for exponential */
 extern double Accuracy;      /* accuracy of the machine */
-#endif /* ACC_H */
+
+extern void evalAccLimits(void);
+
+#endif /* ACCURACY_H */
