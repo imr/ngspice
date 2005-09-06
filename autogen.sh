ae5debfbe1ae98fc31cce396304854ed1ebaa30c
sjborley <sjborley>
2005-09-06 20:28:26 +0000

    Now gives more relevant advice when a required program is missing.

--- autogen.sh
+++ autogen.sh
@@ -10,24 +10,24 @@ DIE=0
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
 	echo
 	echo "You must have autoconf installed to compile $PROJECT."
-	echo "Download the appropriate package for your distribution,"
-	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+	echo "See http://www.gnu.org/software/automake/"
+	echo "(newest stable release is recommended)"
 	DIE=1
 }
 
 (libtoolize --version) < /dev/null > /dev/null 2>&1 || {
 	echo
 	echo "You must have libtool installed to compile $PROJECT."
-	echo "Get ftp://alpha.gnu.org/gnu/libtool-1.0h.tar.gz"
-	echo "(or a newer version if it is available)"
+	echo "See http://www.gnu.org/software/libtool/"
+	echo "(newest stable release is recommended)"
 	DIE=1
 }
 
 (automake --version) < /dev/null > /dev/null 2>&1 || {
 	echo
 	echo "You must have automake installed to compile $PROJECT."
-	echo "Get ftp://ftp.cygnus.com/pub/home/tromey/automake-1.2d.tar.gz"
-	echo "(or a newer version if it is available)"
+	echo "See http://www.gnu.org/software/automake/"
+	echo "(newest stable release is recommended)"
 	DIE=1
 }
 
