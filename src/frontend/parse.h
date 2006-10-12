a1234532958df534380f790ddd0c52ddff173aac
danmc <danmc>
2006-10-12 02:51:13 +0000

    avoid multiple definitions for free_pnode()

--- src/frontend/parse.h
+++ src/frontend/parse.h
@@ -11,7 +11,9 @@
 #include <wordlist.h>
 
 struct pnode * ft_getpnames(wordlist *wl, bool check);
+#ifndef free_pnode
 #define free_pnode(ptr)  free_pnode_x(ptr); ptr=NULL;
+#endif
 void free_pnode_x(struct pnode *t);
 
 
