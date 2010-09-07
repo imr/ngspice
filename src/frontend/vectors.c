113f4ca13d4405c1eea07ef051c25a7ad54ee7c9
h_vogt <h_vogt>
2010-09-07 08:52:21 +0000

    plot [alli | allv | ally]

--- src/frontend/vectors.c
+++ src/frontend/vectors.c
@@ -56,6 +56,60 @@ findvec(char *word, struct plot *pl)
         return (newv);
     }
 
+    if (cieq(word, "allv")) {
+        for (d = pl->pl_dvecs; d; d = d->v_next) {
+            if ((d->v_flags & VF_PERMANENT) && (d->v_type == 3)) {
+                if (d->v_link2) {
+                    v = vec_copy(d);
+                    vec_new(v);
+                } else
+                    v = d;
+                if (end)
+                    end->v_link2 = v;
+                else
+                    newv = v;
+                end = v;
+            }
+        }
+        return (newv);
+    }
+
+    if (cieq(word, "alli")) {
+        for (d = pl->pl_dvecs; d; d = d->v_next) {
+            if ((d->v_flags & VF_PERMANENT) && (d->v_type == 4)) {
+                if (d->v_link2) {
+                    v = vec_copy(d);
+                    vec_new(v);
+                } else
+                    v = d;
+                if (end)
+                    end->v_link2 = v;
+                else
+                    newv = v;
+                end = v;
+            }
+        }
+        return (newv);
+    }
+
+    if (cieq(word, "ally")) {
+        for (d = pl->pl_dvecs; d; d = d->v_next) {
+            if ((d->v_flags & VF_PERMANENT) && (!cieq(d->v_name, pl->pl_scale->v_name))) {
+                if (d->v_link2) {
+                    v = vec_copy(d);
+                    vec_new(v);
+                } else
+                    v = d;
+                if (end)
+                    end->v_link2 = v;
+                else
+                    newv = v;
+                end = v;
+            }
+        }
+        return (newv);
+    }
+
     for (d = pl->pl_dvecs; d; d = d->v_next)
         if (cieq(word, d->v_name) && (d->v_flags & VF_PERMANENT))
             break;
