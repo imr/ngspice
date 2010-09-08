606b974c7da902e0e143eeeca99616d6317b7c91
h_vogt <h_vogt>
2010-09-08 13:42:23 +0000

    prepare for ngspice 22

--- visualc/include/config.h
+++ visualc/include/config.h
@@ -448,7 +448,7 @@
 #define NGSPICEBINDIR "C:/Spice/bin"
 
 /* Define the build date */
-#define NGSPICEBUILDDATE "Sat Jun 12 17:01:55 GMT 2010"
+#define NGSPICEBUILDDATE "Mon Sep 06 23:06:55 GMT 2010"
 
 /* Define the directory for architecture independent data files */
 #define NGSPICEDATADIR "C:/Spice/share/ngspice"
@@ -472,13 +472,13 @@
 #define PACKAGE_NAME "ngspice"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "ngspice 21plus"
+#define PACKAGE_STRING "ngspice 22"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "ngspice"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "21plus"
+#define PACKAGE_VERSION "22"
 
 /* Define if we want predictor algorithm */
 /* #undef PREDICTOR */
@@ -533,7 +533,7 @@
 /* #undef TM_IN_SYS_TIME */
 
 /* Version number of package */
-#define VERSION "21"
+#define VERSION "22"
 
 /* Define if we want spice2 sensitivity analysis */
 /* #undef WANT_SENSE2 */
