cef8a530af59378cbb6e947e82d042980b4dcd4c
rlar <rlar>
2010-10-08 18:01:40 +0000

    com_dotmeasure() prototype

--- src/frontend/com_measure2.h
+++ src/frontend/com_measure2.h
@@ -7,4 +7,6 @@ extern int measure_get_precision(void) ;
 extern int get_measure2(wordlist *wl,double *result,char *out_line, bool auto_check) ;
 extern int measure_extract_variables( char *line ) ;
 
+void com_dotmeasure(wordlist *wl);
+
 #endif
