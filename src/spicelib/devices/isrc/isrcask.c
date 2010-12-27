fbf570878640bd6e2a388d533740dd163543f83a
rlar <rlar>
2010-12-27 09:40:58 +0000

    bug fix, missing isrc trnoise ask function   breaks tests/vbic/diffamp.cir (infinite recursion of error messages)

--- src/spicelib/devices/isrc/isrcask.c
+++ src/spicelib/devices/isrc/isrcask.c
@@ -46,6 +46,7 @@ ISRCask(CKTcircuit *ckt, GENinstance *inst, int which, IFvalue *value, IFvalue *
         case ISRC_PWL:
         case ISRC_SFFM:
 	case ISRC_AM:
+        case ISRC_TRNOISE:
         case ISRC_FCN_COEFFS:
             temp = value->v.numValue = here->ISRCfunctionOrder;
             value->v.vec.rVec = TMALLOC(double, here->ISRCfunctionOrder);
