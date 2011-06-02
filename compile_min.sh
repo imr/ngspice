f9a5d631aee73e15e2e630cee67540d6a20a41a3
h_vogt <h_vogt>
2011-06-02 08:58:30 +0000

    mingw installation

--- compile_min.sh
+++ compile_min.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# ngspice build script for MINGW-w64
+# ngspice build script for MINGW-w64, release version, 32 or 64 bit
 # compile_min.sh
 
 #Procedure:
@@ -14,21 +14,42 @@
 # Options:
 # --adms and --enable-adms will install extra HICUM, EKV and MEXTRAM models via the 
 # adms interface.
+# Please see http://ngspice.sourceforge.net/admshowto.html for more info on adms.
 # CIDER, XSPICE, and OpenMP may be selected at will.
 # --disable-debug will give O2 optimization (versus O0 for debug) and removes all debugging info.
 
-./autogen.sh --adms
-if [ $? -ne 0 ]; then  echo "./autogen.sh failed"; exit 1 ; fi
+
+if test "$1" = "64"; then
+   if [ ! -d "release64" ]; then
+      mkdir release64
+      if [ $? -ne 0 ]; then  echo "mkdir release64 failed"; exit 1 ; fi
+   fi   
+else
+   if [ ! -d "release" ]; then
+      mkdir release
+      if [ $? -ne 0 ]; then  echo "mkdir release failed"; exit 1 ; fi
+   fi
+fi
+
+# If compiling sources from CVS, you may need to uncomment the following two lines:
+#./autogen.sh --adms
+#if [ $? -ne 0 ]; then  echo "./autogen.sh failed"; exit 1 ; fi
 
 echo
 if test "$1" = "64"; then
-echo "configuring for 64 bit"
-echo
-./configure --with-windows --enable-xspice --enable-cider --enable-openmp --enable-adms --disable-debug  prefix="C:/Spice64" CFLAGS="-m64" LDFLAGS="-m64"
+   cd release64
+   if [ $? -ne 0 ]; then  echo "cd release64 failed"; exit 1 ; fi
+  echo "configuring for 64 bit"
+  echo
+# You may add  --enable-adms to the following command for adding adms generated devices 
+  ../configure --with-windows --enable-xspice --enable-cider --enable-openmp --disable-debug  prefix="C:/Spice64" CFLAGS="-m64" LDFLAGS="-m64"
 else
-echo "configuring for 32 bit"
-echo
-./configure --with-windows --enable-xspice --enable-cider --enable-openmp --enable-adms --disable-debug CFLAGS="-m32" LDFLAGS="-m32"
+   cd release
+   if [ $? -ne 0 ]; then  echo "cd release failed"; exit 1 ; fi
+  echo "configuring for 32 bit"
+  echo
+# You may add  --enable-adms to the following command for adding adms generated devices 
+  ../configure --with-windows --enable-xspice --enable-cider --enable-openmp --disable-debug CFLAGS="-m32" LDFLAGS="-m32"
 fi
 if [ $? -ne 0 ]; then  echo "./configure failed"; exit 1 ; fi
 
