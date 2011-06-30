1041ecf8d0da9cdde6c240016d792583bdf3d33c
rlar <rlar>
2011-06-30 16:33:10 +0000

    suprmitf.c, uninitialized variable warning

--- src/ciderlib/support/suprmitf.c
+++ src/ciderlib/support/suprmitf.c
@@ -202,11 +202,17 @@ SUPbinRead(char *inFile, float *x, float *conc, int *impId, int *numNod)
   fclose( fpSuprem );
 
 /* shift silicon layer to beginning of array */
-  for ( j=0; j < numLay; j++ ) {
-    if (matTyp[ j ] == 1) {
-      siIndex = j;
-    }
+  for ( j=numLay; --j >= 0; )
+    if (matTyp[ j ] == 1)
+        break;
+
+  if(j < 0) {
+      fprintf(stderr, "internal error in %s, bye !\n", __FUNCTION__);
+      exit(1);
   }
+
+  siIndex = j;
+
   offset = topNod[ siIndex ] - 1;
   numGrid -= offset;
   xStart = x[1 + offset];
@@ -332,11 +338,17 @@ SUPascRead(char *inFile, float *x, float *conc, int *impId, int *numNod)
 
 
 /* shift silicon layer to beginning of array */
-  for ( j=0; j < numLay; j++ ) {
-    if (matTyp[ j ] == 1) {
-      siIndex = j;
-    }
+  for ( j=numLay; --j >= 0; )
+    if (matTyp[ j ] == 1)
+        break;
+
+  if(j < 0) {
+      fprintf(stderr, "internal error in %s, bye !\n", __FUNCTION__);
+      exit(1);
   }
+
+  siIndex = j;
+
   offset = topNod[ siIndex ] - 1;
   numGrid -= offset;
   xStart = x[1 + offset];
