f2f43bcce7f240a13ca0a85b835ad2173ef74a89
rlar <rlar>
2012-07-31 20:38:30 +0200

    lexical #1/19, use wl_cons

--- src/frontend/parser/lexical.c
+++ src/frontend/parser/lexical.c
@@ -75,10 +75,9 @@ static int numeofs = 0;
  */
 
 #define newword cw->wl_word = copy(buf); \
-        cw->wl_next = alloc(struct wordlist); \
+        cw->wl_next = wl_cons(NULL, NULL); \
         cw->wl_next->wl_prev = cw; \
         cw = cw->wl_next; \
-        cw->wl_next = NULL; \
         bzero(buf, NEW_BSIZE_SP); \
         i = 0;
 
