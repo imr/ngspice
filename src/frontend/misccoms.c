998c7f2934145c33c40e0555377d8fc2ffe45bd9
rlar <rlar>
2012-09-25 21:31:45 +0200

    disable some more cleanup code in com_quit()
    destroy_ivars() is reported to freeze

--- src/frontend/misccoms.c
+++ src/frontend/misccoms.c
@@ -90,12 +90,16 @@ com_quit(wordlist *wl)
 
     /* then go away */
 
+#if 0
     cp_destroy_keywords();
     destroy_ivars();
+#endif
 
     byemesg();
+#if 0
     destroy_const_plot();
     spice_destroy_devices();
+#endif
     exit(exitcode);
 }
 
