1163be2c0c3279fa99e244c412f55543cdcb4927
rlar <rlar>
2012-10-17 19:32:31 +0200

    INPdevParse(), fix a memory leak

--- src/spicelib/parser/inpdpar.c
+++ src/spicelib/parser/inpdpar.c
@@ -66,6 +66,7 @@ char *INPdevParse(char **line, CKTcircuit *ckt, int dev, GENinstance *fast,
 		error = ft_sim->setInstanceParm (ckt, fast,
 						 ft_sim->devices[dev]->instanceParms[i].id,
 						 val, NULL);
+		tfree(val->v.vec.rVec);
 		if (error) {
 		    rtn = INPerror(error);
 		    goto quit;
