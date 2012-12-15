e67e9da2029c19a8c3493d3029dca3671b8aa075
rlar <rlar>
2012-12-15 21:51:59 +0100

    inperror.c, release errMsg when it has been consumed

--- src/spicelib/parser/inperror.c
+++ src/spicelib/parser/inperror.c
@@ -33,6 +33,7 @@ char *INPerror(int type)
     /*CDHW Lots of things set errMsg but it is never used  so let's hack it in CDHW*/
     if ( errMsg ) {
         val = errMsg;
+        errMsg = NULL;
     } else
         /*CDHW end of hack CDHW*/
         val = copy(SPerror(type));
