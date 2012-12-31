482a45b1b1420a92d237c847bcf25bd2c35698d2
rlar <rlar>
2012-12-31 11:17:40 +0100

    compare a little bit less when running `make check'
    windows ngspice emmits a user notice "binary raw file" when running ngspice
    don't compare that

--- tests/bin/check.sh
+++ tests/bin/check.sh
@@ -17,7 +17,7 @@ fi
 SPICE=$1
 TEST=$2
 
-FILTER="CPU|Dynamic|Note|Circuit|Trying|Reference|Date|Doing|---|v-sweep|time|est|Error|Warning|Data|Index|trans|acan|oise|nalysis|ole|Total|memory|urrent|Got|Added|BSIM|bsim|B4SOI|b4soi|codemodel"
+FILTER="CPU|Dynamic|Note|Circuit|Trying|Reference|Date|Doing|---|v-sweep|time|est|Error|Warning|Data|Index|trans|acan|oise|nalysis|ole|Total|memory|urrent|Got|Added|BSIM|bsim|B4SOI|b4soi|codemodel|^binary raw file"
 
 testname=`basename $TEST .cir`
 testdir=`dirname $TEST`
@@ -27,8 +27,8 @@ HOST_TYPE=`uname -srvm`
 case $HOST_TYPE in
     MINGW32*)
       $SPICE --batch $testdir/$testname.cir -o $testname.test &&\
-      sed -e 's/e-000/e+000/g' $testname.test | sed 's/e-0/e-/g' | sed 's/e+0/e+/g' | egrep -v $FILTER > $testname.test_tmp &&\
-      sed -e 's/-0$/ 0/g' $testdir/$testname.out | egrep -v $FILTER > $testname.out_tmp
+      sed -e 's/e-000/e+000/g' $testname.test | sed 's/e-0/e-/g' | sed 's/e+0/e+/g' | egrep -v "$FILTER" > $testname.test_tmp &&\
+      sed -e 's/-0$/ 0/g' $testdir/$testname.out | egrep -v "$FILTER" > $testname.out_tmp
       if diff -B -w -u $testname.out_tmp $testname.test_tmp; then
           rm $testname.test $testname.test_tmp $testname.out_tmp
           exit 0
@@ -39,8 +39,8 @@ case $HOST_TYPE in
       ;;
     Linux*|Darwin*|CYGWIN*)
       $SPICE --batch $testdir/$testname.cir >$testname.test &&\
-      egrep -v $FILTER $testname.test > $testname.test_tmp &&\
-      egrep -v $FILTER $testdir/$testname.out > $testname.out_tmp
+      egrep -v "$FILTER" $testname.test > $testname.test_tmp &&\
+      egrep -v "$FILTER" $testdir/$testname.out > $testname.out_tmp
       if diff -B -w -u $testname.out_tmp $testname.test_tmp; then
           rm $testname.test $testname.test_tmp $testname.out_tmp
           exit 0
@@ -49,8 +49,8 @@ case $HOST_TYPE in
       ;;
     SunOS*)
       $SPICE --batch $testdir/$testname.cir >$testname.test &&\
-      sed -e '/^$/d' $testname.test | egrep -v $FILTER > $testname.test_tmp &&\
-      sed -e '/^$/d' $testdir/$testname.out | egrep -v $FILTER > $testname.out_tmp
+      sed -e '/^$/d' $testname.test | egrep -v "$FILTER" > $testname.test_tmp &&\
+      sed -e '/^$/d' $testdir/$testname.out | egrep -v "$FILTER" > $testname.out_tmp
       if diff -b -w $testname.out_tmp $testname.test_tmp; then
           rm $testname.test $testname.test_tmp $testname.out_tmp
           exit 0
