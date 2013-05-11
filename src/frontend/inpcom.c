4d4f05c3802d8d4aa1a3e04cebd6098f6ec3053b
h_vogt <h_vogt>
2013-05-11 16:06:58 +0200

    inpcom.c: no multiplier 'm' added to V, I lines in subckt

--- src/frontend/inpcom.c
+++ src/frontend/inpcom.c
@@ -2648,8 +2648,9 @@ inp_fix_subckt_multiplier(struct line *subckt_card,
     for (card = subckt_card->li_next;
          card != NULL && !ciprefix(".ends", card->li_line);
          card = card->li_next) {
-        /* no 'm' for B line or comment line */
-        if ((*(card->li_line) == '*') || (*(card->li_line) == 'b'))
+        /* no 'm' for B, I, V or comment line */
+        if ((*(card->li_line) == '*') || (*(card->li_line) == 'b') || (*(card->li_line) == 'v')
+             || (*(card->li_line) == 'i'))
             continue;
         /* no 'm' for model cards */
         if (ciprefix(".model", card->li_line))
