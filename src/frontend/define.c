67301b25fef8b5360fa20c5a17114be1afab2227
rlar <rlar>
2015-11-07 20:02:20 +0100

    frontend/define.c, ntharg(), #5/15 make use of `fact: num >= 1'

--- src/frontend/define.c
+++ src/frontend/define.c
@@ -410,8 +410,7 @@ ntharg(int num, struct pnode *args)
     // fact: num >= 1 for all known invocations of ntharg()
         while (--num > 0) {
             if (args && args->pn_op && (args->pn_op->op_num != PT_OP_COMMA)) {
-                // fact: num >= 1 because of `while' condition
-                if (num == 1)
+                if (num <= 1)
                     break;
                 return NULL;
             }
