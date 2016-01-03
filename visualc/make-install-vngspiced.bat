567203d3ae45bc0b73404079ed55bdf6a28c78ee
h_vogt <h_vogt>
2016-01-03 11:25:46 +0100

    make-install-vngspiced.bat: enable fftw in debug mode

--- visualc/make-install-vngspiced.bat
+++ visualc/make-install-vngspiced.bat
@@ -11,7 +11,6 @@ mkdir c:\Spiced\bin
 mkdir c:\Spiced\lib\ngspice
 mkdir C:\Spiced\share\ngspice\scripts
 
-copy .\vngspice\Debug.Win32\ngspice.exe c:\Spiced\bin\ngspice.exe
 copy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x86\Microsoft.VC140.OPENMP\vcomp140.dll" c:\Spiced\bin\vcomp140.dll
 copy .\codemodels\Win32\Debug\analog.cm c:\Spiced\lib\ngspice\analog.cm
 copy .\codemodels\Win32\Debug\digital.cm c:\Spiced\lib\ngspice\digital.cm
@@ -20,3 +19,14 @@ copy .\codemodels\Win32\Debug\xtraevt.cm c:\Spiced\lib\ngspice\xtraevt.cm
 copy .\codemodels\Win32\Debug\xtradev.cm c:\Spiced\lib\ngspice\xtradev.cm
 copy .\codemodels\Win32\Debug\spice2poly.cm c:\Spiced\lib\ngspice\spice2poly.cm
 copy .\spinitd C:\Spiced\share\ngspice\scripts\spinit
+
+if "%1" == "fftw" goto copy2
+
+copy .\vngspice\Debug.Win32\ngspice.exe c:\Spiced\bin\ngspice.exe
+goto end
+
+:copy2
+copy .\vngspice-fftw\Debug.Win32\ngspice.exe c:\Spiced\bin\ngspice.exe
+copy "..\..\fftw-3.3.4-dll32\libfftw3-3.dll" "C:\Spiced\bin\libfftw3-3.dll"
+
+:end
