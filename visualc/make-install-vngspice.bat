3af3e72121c058115e15cf069e63ed7a62b65cf2
h_vogt <h_vogt>
2016-01-03 15:36:14 +0100

    *.bat, *fftw.vcproj: enable 64 Bit installation

--- visualc/make-install-vngspice.bat
+++ visualc/make-install-vngspice.bat
@@ -7,6 +7,9 @@ REM start /WAIT "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE
 
 REM start /w "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.com" "D:\Spice_general\ngspice\visualc\vngspice.sln" /Rebuild 
 
+if "%1" == "64" goto b64
+
+if "%2" == "64" goto b64
 
 mkdir c:\Spice\bin
 mkdir c:\Spice\lib\ngspice
@@ -22,6 +25,7 @@ copy .\codemodels\Win32\Release\spice2poly.cm c:\Spice\lib\ngspice\spice2poly.cm
 copy .\spinit C:\Spice\share\ngspice\scripts\spinit
 
 if "%1" == "fftw" goto copy2
+if "%2" == "fftw" goto copy2
 
 copy .\vngspice\ReleaseOMP.Win32\ngspice.exe c:\Spice\bin\ngspice.exe
 goto end
@@ -29,5 +33,32 @@ goto end
 :copy2
 copy .\vngspice-fftw\ReleaseOMP.Win32\ngspice.exe c:\Spice\bin\ngspice.exe
 copy "..\..\fftw-3.3.4-dll32\libfftw3-3.dll" "C:\Spice\bin\libfftw3-3.dll"
+goto end
+
+:b64
+
+mkdir c:\Spice64\bin
+mkdir c:\Spice64\lib\ngspice
+mkdir C:\Spice64\share\ngspice\scripts
+
+copy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x64\Microsoft.VC140.OPENMP\vcomp140.dll" c:\Spice64\bin\vcomp140.dll
+copy .\codemodels\x64\Release\analog64.cm c:\Spice64\lib\ngspice\analog.cm
+copy .\codemodels\x64\Release\digital64.cm c:\Spice64\lib\ngspice\digital.cm
+copy .\codemodels\x64\Release\table64.cm c:\Spice64\lib\ngspice\table.cm
+copy .\codemodels\x64\Release\xtraevt64.cm c:\Spice64\lib\ngspice\xtraevt.cm
+copy .\codemodels\x64\Release\xtradev64.cm c:\Spice64\lib\ngspice\xtradev.cm
+copy .\codemodels\x64\Release\spice2poly64.cm c:\Spice64\lib\ngspice\spice2poly.cm
+copy .\spinit64 C:\Spice64\share\ngspice\scripts\spinit
+
+if "%1" == "fftw" goto copy2-64
+if "%2" == "fftw" goto copy2-64
+
+copy .\vngspice\ReleaseOMP.x64\ngspice.exe c:\Spice64\bin\ngspice.exe
+goto end
+
+:copy2-64
+copy .\vngspice-fftw\ReleaseOMP.x64\ngspice.exe c:\Spice64\bin\ngspice.exe
+copy "..\..\fftw-3.3.4-dll64\libfftw3-3.dll" "C:\Spice64\bin\libfftw3-3.dll"
+goto end
 
 :end
--- visualc/make-install-vngspiced.bat
+++ visualc/make-install-vngspiced.bat
@@ -7,6 +7,10 @@ REM start /WAIT "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE
 
 REM start /w "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.com" "D:\Spice_general\ngspice\visualc\vngspice.sln" /Rebuild 
 
+if "%1" == "64" goto b64
+
+if "%2" == "64" goto b64
+
 mkdir c:\Spiced\bin
 mkdir c:\Spiced\lib\ngspice
 mkdir C:\Spiced\share\ngspice\scripts
@@ -28,5 +32,31 @@ goto end
 :copy2
 copy .\vngspice-fftw\Debug.Win32\ngspice.exe c:\Spiced\bin\ngspice.exe
 copy "..\..\fftw-3.3.4-dll32\libfftw3-3.dll" "C:\Spiced\bin\libfftw3-3.dll"
+
+:b64
+
+mkdir c:\Spice64d\bin
+mkdir c:\Spice64d\lib\ngspice
+mkdir C:\Spice64d\share\ngspice\scripts
+
+copy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x64\Microsoft.VC140.OPENMP\vcomp140.dll" c:\Spice64d\bin\vcomp140.dll
+copy .\codemodels\x64\Debug\analog64.cm c:\Spice64d\lib\ngspice\analog.cm
+copy .\codemodels\x64\Debug\digital64.cm c:\Spice64d\lib\ngspice\digital.cm
+copy .\codemodels\x64\Debug\table64.cm c:\Spice64d\lib\ngspice\table.cm
+copy .\codemodels\x64\Debug\xtraevt64.cm c:\Spice64d\lib\ngspice\xtraevt.cm
+copy .\codemodels\x64\Debug\xtradev64.cm c:\Spice64d\lib\ngspice\xtradev.cm
+copy .\codemodels\x64\Debug\spice2poly64.cm c:\Spice64d\lib\ngspice\spice2poly.cm
+copy .\spinitd64 C:\Spice64d\share\ngspice\scripts\spinit
+
+if "%1" == "fftw" goto copy2-64
+
+if "%2" == "fftw" goto copy2-64
+
+copy .\vngspice\Debug.x64\ngspice.exe c:\Spice64d\bin\ngspice.exe
+goto end
+
+:copy2-64
+copy .\vngspice-fftw\Debug.x64\ngspice.exe c:\Spice64d\bin\ngspice.exe
+copy "..\..\fftw-3.3.4-dll64\libfftw3-3.dll" "C:\Spice64d\bin\libfftw3-3.dll"
 
 :end
