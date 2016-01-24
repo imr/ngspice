72033467c97c9614c10108b9eb17af008cbe7db2
rlar <rlar>
2016-01-24 21:06:04 +0100

    aux-cfunc.bat, update path to the new directory

--- visualc/XSPICE/aux-cfunc.bat
+++ visualc/XSPICE/aux-cfunc.bat
@@ -2,18 +2,19 @@ rem invoke as
 rem    .\aux-cfunc.bat analog
 
 set sub=%1
+set cmpp=.\cmpp\Release.Win32\cmpp.exe
 
 set CMPP_IDIR=../../src/xspice/icm/%sub%
 set CMPP_ODIR=icm/%sub%
 if not exist icm\%sub% mkdir icm\%sub%
-.\bin\cmpp -lst
+%cmpp% -lst
 
 for /F %%n in (..\..\src\xspice\icm\%sub%\modpath.lst) do (
   set CMPP_IDIR=../../src/xspice/icm/%sub%/%%n
   set CMPP_ODIR=icm/%sub%/%%n
   if not exist icm\%sub%\%%n mkdir icm\%sub%\%%n
-  .\bin\cmpp -ifs
-  .\bin\cmpp -mod
+  %cmpp% -ifs
+  %cmpp% -mod
   pushd icm\%sub%\%%n
   if exist %%n-cfunc.c del %%n-cfunc.c
   if exist %%n-ifspec.c del %%n-ifspec.c
