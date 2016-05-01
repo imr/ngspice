f3b970ecc6236995237ba266e688725fc91d885b
rlar <rlar>
2016-05-01 15:53:22 +0200

    nupa_substitute(), cleanup

--- src/frontend/numparam/xpressn.c
+++ src/frontend/numparam/xpressn.c
@@ -1230,8 +1230,8 @@ nupa_substitute(dico_t *dico, const char *s, char *r)
     const char *kptr;
 
     while ((s < s_end) && !err) {
-        s++;
-        c = s[-1];
+
+        c = *s++;
 
         if (c == '{') {
             /* try ps expression syntax */
@@ -1240,8 +1240,8 @@ nupa_substitute(dico_t *dico, const char *s, char *r)
 
             do
             {
-                kptr++;
-                d = kptr[-1];
+                d = *kptr++;
+
                 if (d == '{')
                     nnest++;
                 else if (d == '}')
@@ -1290,11 +1290,12 @@ nupa_substitute(dico_t *dico, const char *s, char *r)
 
                 do
                 {
-                    kptr++;
-                    if (kptr > s_end)
+                    if (kptr >= s_end)
                         d = '\0';
                     else
-                        d = kptr[-1];
+                        d = *kptr;
+
+                    kptr++;
 
                     if (d == '(')
                         level++;
@@ -1318,11 +1319,12 @@ nupa_substitute(dico_t *dico, const char *s, char *r)
 
                 do
                 {
-                    kptr++;
-                    if (kptr > s_end)
+                    if (kptr >= s_end)
                         d = '\0';
                     else
-                        d = kptr[-1];
+                        d = *kptr;
+
+                    kptr++;
 
                 } while ((kptr <= s_end) && (d > ' '));
 
