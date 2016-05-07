7334e64cd4ec6d9052fb8e57a1f4a67bac50cb23
h_vogt <h_vogt>
2016-05-07 18:35:59 +0200

    .model treatment in fcn comment_out_unused_subckt_models() removed

--- src/frontend/inpcom.c
+++ src/frontend/inpcom.c
@@ -1735,6 +1735,7 @@ nlist_find(const struct nlist *nlist, const char *name)
 }
 
 
+#if 0
 static const char *
 nlist_model_find(const struct nlist *nlist, const char *name)
 {
@@ -1744,6 +1745,7 @@ nlist_model_find(const struct nlist *nlist, const char *name)
             return nlist->names[i];
     return NULL;
 }
+#endif
 
 
 static void
@@ -1946,6 +1948,7 @@ comment_out_unused_subckt_models(struct line *start_card)
 
         if (remove_subckt)
             *line = '*';
+#if 0
         else if (has_models &&
                  (ciprefix(".model", line) || ciprefix(".cmodel", line)))
         {
@@ -1966,6 +1969,7 @@ comment_out_unused_subckt_models(struct line *start_card)
             tfree(model_type);
             tfree(model_name);
         }
+#endif
     }
 
     nlist_destroy(used_subckts);
