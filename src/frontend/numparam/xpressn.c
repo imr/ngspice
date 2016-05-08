65619835bc36c8fa05f54efe387b9b150a47321c
rlar <rlar>
2016-05-08 18:44:59 +0200

    defsubckt(), fix lvalue

--- src/frontend/numparam/xpressn.c
+++ src/frontend/numparam/xpressn.c
@@ -536,8 +536,7 @@ defsubckt(dico_t *dico, struct card *card)
     bool err;
     int i, j;
 
-    const char * const ls_ptr;
-    (ls_ptr - s) = (int) strlen(s);
+    const char * const ls_ptr = s + strlen(s);
     i = 0;
 
     while ((i < (ls_ptr - s)) && (s[i] != '.'))
