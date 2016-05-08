ece6da663dd4bf517f678bf716dc7a830e4bbd69
rlar <rlar>
2016-05-08 19:00:19 +0200

    defsubckt(), check `s' beeing nowhere used anymore

--- src/frontend/numparam/xpressn.c
+++ src/frontend/numparam/xpressn.c
@@ -507,13 +507,13 @@ defsubckt(dico_t *dico, struct card *card, nupa_type categ)
    to enter subcircuit (categ=U) and model (categ=O) names
 */
 {
-    const char * const s = card->line;
+    const char * const s_ = card->line;
     int w = card->linenum;
 
     bool err;
 
     const char *i_ptr, *j_ptr;
-    i_ptr = s;
+    i_ptr = s_;
 
     while (*i_ptr && (*i_ptr != '.'))
         i_ptr++;                /* skip 1st dotword */
