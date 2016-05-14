93e422ad664cae0c2b53322995a265d5d93d3712
rlar <rlar>
2016-05-14 13:44:56 +0200

    nupa_substitute(), move final kptr incrementation out of the loop

--- src/frontend/numparam/xpressn.c
+++ src/frontend/numparam/xpressn.c
@@ -1233,14 +1233,13 @@ nupa_substitute(dico_t *dico, const char *s, char *r)
                     nnest--;
 
                 if (nnest == 0) {
-                    kptr++;
                     break;
                 }
                 if (d == '\0') {
-                    kptr++;
                     break;
                 }
             }
+            kptr++;
 
             if (d == '\0') {
                 err = message(dico, "Closing \"}\" not found.\n");
@@ -1291,14 +1290,13 @@ nupa_substitute(dico_t *dico, const char *s, char *r)
                         level--;
 
                     if (kptr >= s_end) {
-                        kptr++;
                         break;
                     }
                     if ((d == ')') && (level <= 0)) {
-                        kptr++;
                         break;
                     }
                 }
+                kptr++;
 
                 if (kptr > s_end) {
                     err = message(dico, "Closing \")\" not found.\n");
@@ -1320,14 +1318,13 @@ nupa_substitute(dico_t *dico, const char *s, char *r)
                         d = *kptr;
 
                     if (kptr >= s_end) {
-                        kptr++;
                         break;
                     }
                     if (d <= ' ') {
-                        kptr++;
                         break;
                     }
                 }
+                kptr++;
 
                 err = evaluate_variable(dico, &qstr, s - 1, kptr - 1);
                 s = kptr - 1;
