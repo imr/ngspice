795083a0b26bd0ae870cfc8fc4019ef110d6875e
h_vogt <h_vogt>
2016-06-06 22:47:56 +0200

    inpcom.c: remove white spaces in command files debug output of command files

--- src/frontend/inpcom.c
+++ src/frontend/inpcom.c
@@ -528,6 +528,8 @@ inp_readall(FILE *fp, char *dir_name, bool comfile, bool intfile, bool *expr_w_t
     struct line *cc;
     struct inp_read_t rv;
 
+    static int tpr = 0;
+
     num_libraries = 0;
     inp_compat_mode = ngspice_compat_mode();
 
@@ -578,9 +580,9 @@ inp_readall(FILE *fp, char *dir_name, bool comfile, bool intfile, bool *expr_w_t
         inp_fix_param_values(working);
 
         inp_reorder_params(subckt_w_params, cc);
-//      tprint(working, 1);
+//      tprint(working, tpr++);
         inp_fix_inst_calls_for_numparam(subckt_w_params, working);
-//      tprint(working, 2);
+//      tprint(working, tpr++);
 
         delete_names(subckt_w_params);
         subckt_w_params = NULL;
@@ -657,6 +659,13 @@ inp_readall(FILE *fp, char *dir_name, bool comfile, bool intfile, bool *expr_w_t
             fprintf(stdout, "max line length %d, max subst. per line %d, number of lines %d\n",
                     (int) max_line_length, no_braces, dynmaxline);
         }
+    }
+    /* remove white spaces in command files */
+    else if (comfile && cc) {
+        struct line *working = cc->li_next;
+        inp_remove_excess_ws(working);
+        if (ft_ngdebug)
+            tprint(working, tpr++);
     }
 
     return cc;
