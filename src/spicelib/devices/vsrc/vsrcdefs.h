bb34ec78c896b9a9c2b9c7e2d94f976c276bacd8
rlar <rlar>
2016-06-19 17:49:53 +0200

    fix vsrc for pz analysis

--- src/spicelib/devices/vsrc/vsrcdefs.h
+++ src/spicelib/devices/vsrc/vsrcdefs.h
@@ -83,6 +83,7 @@ typedef struct sVSRCinstance {
     BindElement *VSRCnegIbrBinding ;
     BindElement *VSRCibrNegBinding ;
     BindElement *VSRCibrPosBinding ;
+    BindElement *VSRCibrIbrBinding ;
 #endif
 
 } VSRCinstance ;
