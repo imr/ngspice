ce18d6846fc4a4846d46256a9d87de817493eb13
Francesco Lannutti <nicolati@fastwebnet.it>
2016-06-21 12:53:49 +0200

    KLU Integration from scratch #4, changed files

--- src/spicelib/analysis/cktntask.c
+++ src/spicelib/analysis/cktntask.c
@@ -76,6 +76,11 @@ CKTnewTask(CKTcircuit *ckt, TSKtask **taskPtr, IFuid taskName, TSKtask **defPtr)
         tsk->TSKrelDv           = def->TSKrelDv;
         tsk->TSKnoopac          = def->TSKnoopac;
         tsk->TSKepsmin          = def->TSKepsmin;
+
+#ifdef KLU
+        tsk->TSKkluMODE         = def->TSKkluMODE;
+#endif
+
 #ifdef NEWTRUNC
         tsk->TSKlteReltol       = def->TSKlteReltol;
         tsk->TSKlteAbstol       = def->TSKlteAbstol;
@@ -133,6 +138,10 @@ CKTnewTask(CKTcircuit *ckt, TSKtask **taskPtr, IFuid taskName, TSKtask **defPtr)
         tsk->TSKabsDv           = 0.5;
         tsk->TSKrelDv           = 2.0;
         tsk->TSKepsmin          = 1e-28;
+
+#ifdef KLU
+        tsk->TSKkluMODE         = CKTkluON;
+#endif
 
 #if (1) /*CDHW*/
     }
