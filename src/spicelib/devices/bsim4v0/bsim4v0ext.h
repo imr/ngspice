36489ba1f621fe4c603e82aef8a3bde6592512f5
rlar <rlar>
2016-06-26 12:17:11 +0200

    remove some #ifdef __STDC__ wrappers, and some stray '#'

--- src/spicelib/devices/bsim4v0/bsim4v0ext.h
+++ src/spicelib/devices/bsim4v0/bsim4v0ext.h
@@ -4,7 +4,6 @@ Author: 2000 Weidong Liu.
 File: bsim4v0ext.h
 **********/
 
-#ifdef __STDC__
 extern int BSIM4v0acLoad(GENmodel *,CKTcircuit*);
 extern int BSIM4v0ask(CKTcircuit *,GENinstance*,int,IFvalue*,IFvalue*);
 extern int BSIM4v0convTest(GENmodel *,CKTcircuit*);
@@ -29,27 +28,3 @@ extern int BSIM4v0trunc(GENmodel*,CKTcircuit*,double*);
 extern int BSIM4v0noise(int,int,GENmodel*,CKTcircuit*,Ndata*,double*);
 extern int BSIM4v0unsetup(GENmodel*,CKTcircuit*);
 //extern int BSIM4v0soaCheck(CKTcircuit *, GENmodel *);
-
-#else /* stdc */
-extern int BSIM4v0acLoad();
-extern int BSIM4v0delete();
-extern void BSIM4v0destroy();
-extern int BSIM4v0getic();
-extern int BSIM4v0load();
-extern int BSIM4v0mDelete();
-extern int BSIM4v0ask();
-extern int BSIM4v0mAsk();
-extern int BSIM4v0convTest();
-extern int BSIM4v0temp();
-extern int BSIM4v0mParam();
-extern void BSIM4v0mosCap();
-extern int BSIM4v0param();
-extern int BSIM4v0pzLoad();
-extern int BSIM4v0setup();
-extern int BSIM4v0trunc();
-extern int BSIM4v0noise();
-extern int BSIM4v0unsetup();
-//extern int BSIM4v0soaCheck();
-
-#endif /* stdc */
-
