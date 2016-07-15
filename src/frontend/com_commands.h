cc7099e52e20e53b151b477f2846103196a1dd9a
h_vogt <h_vogt>
2016-07-16 01:09:27 +0200

    new command 'alterparam', to be used in conjunction with 'mc_source'
    add command 'alterparam paramname=pvalue'
      to change a global .param
    
    add command 'alterparam subcktname pname=vpval'
      to change a parameter inside of a subcircuit

--- src/frontend/com_commands.h
+++ src/frontend/com_commands.h
@@ -5,6 +5,7 @@ void com_showmod(wordlist *wl);
 void com_show(wordlist *wl);
 void com_alter(wordlist *wl);
 void com_altermod(wordlist *wl);
+void com_alterparam(wordlist *wl);
 void com_meas(wordlist *wl);
 void com_sysinfo(wordlist *wl);
 void com_check_ifparm(wordlist *wl);
