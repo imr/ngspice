eb65ff8ec89ab68a6a1bd062476244ab29bf39c3
h_vogt <h_vogt>
2016-08-04 19:30:47 +0200

    inp.c, xpressn.c, cmath2.c, apply gauss1() instead of gauss0() -> reproducible start value if 'set rndseed=val' is given.

--- src/frontend/inp.c
+++ src/frontend/inp.c
@@ -1897,7 +1897,7 @@ agauss(double nominal_val, double abs_variation, double sigma)
 {
     double stdvar;
     stdvar = abs_variation / sigma;
-    return (nominal_val + stdvar * gauss0());
+    return (nominal_val + stdvar * gauss1());
 }
 
 
@@ -1906,7 +1906,7 @@ gauss(double nominal_val, double rel_variation, double sigma)
 {
     double stdvar;
     stdvar = nominal_val * rel_variation / sigma;
-    return (nominal_val + stdvar * gauss0());
+    return (nominal_val + stdvar * gauss1());
 }
 
 
