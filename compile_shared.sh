6d80780a37db905000ca0aef4c7d981fd8841209
h_vogt <h_vogt>
2016-10-16 16:09:04 +0200

    configure.ac, library version info added here may be overridden by ./configure

--- compile_shared.sh
+++ compile_shared.sh
@@ -45,17 +45,8 @@ if [ $? -ne 0 ]; then  echo "./autogen.sh failed"; exit 1 ; fi
 #./autogen.sh --adms
 #if [ $? -ne 0 ]; then  echo "./autogen.sh failed"; exit 1 ; fi
 
-# In the following ../configure commands you will find an additional entry to the CFLAGS
-# '-fno-omit-frame-pointer'. This entry compensates for a compiler bug of actual mingw gcc 4.6.1. 
-
-#libtool versioning current:revision:age
-#current The most recent interface number that this library implements.
-#revision The implementation number of the current interface.
-#age The difference between the newest and oldest interfaces that this library implements.
-#In other words, the library implements all the interface numbers in the
-#range from numbers (current - age) to current.
-#libtool in msys2 generates a single version number (current - age)
-#libtool in LINUX generates the numbers as: (current-age).age.revision
+# You may add LIB_VERSION="a:b:c" to the ../configure statement to override LIBRARY_VERSION
+# in configure.ac, where you can find details on the selection of a, b, and c.
 
 echo
 if test "$1" = "64"; then
@@ -64,14 +55,14 @@ if test "$1" = "64"; then
   echo "configuring for 64 bit"
   echo
 # You may add  --enable-adms to the following command for adding adms generated devices 
-  ../configure --with-ngshared --enable-xspice --enable-cider --enable-openmp --enable-relpath --disable-debug prefix="C:/Progra~1/KiCad" CFLAGS="-m64 -O2"  LDFLAGS="-m64 -s" LIB_VERSION="1:0:0"
+  ../configure --with-ngshared --enable-xspice --enable-cider --enable-openmp --enable-relpath --disable-debug prefix="C:/Progra~1/KiCad" CFLAGS="-m64 -O2"  LDFLAGS="-m64 -s"
 else
    cd release-sh
    if [ $? -ne 0 ]; then  echo "cd release-sh failed"; exit 1 ; fi
   echo "configuring for 32 bit"
   echo
 # You may add  --enable-adms to the following command for adding adms generated devices 
-  ../configure --with-ngshared  --enable-xspice --enable-cider --enable-openmp --enable-relpath --disable-debug prefix="C:/Spice" CFLAGS="-m32 -O2" LDFLAGS="-m32 -s" LIB_VERSION="1:0:0"
+  ../configure --with-ngshared  --enable-xspice --enable-cider --enable-openmp --enable-relpath --disable-debug prefix="C:/Spice" CFLAGS="-m32 -O2" LDFLAGS="-m32 -s"
 fi
 if [ $? -ne 0 ]; then  echo "../configure failed"; exit 1 ; fi
 
