bd50814677adbbf262a7afd326370094bc4ce2ee
rlar <rlar>
2016-10-30 11:32:55 +0100

    drop 'depreciated_skip..()'

--- src/include/ngspice/stringskip.h
+++ src/include/ngspice/stringskip.h
@@ -4,8 +4,6 @@
 static inline char *skip_non_ws(const char *s)  { while (*s && !isspace_c(*s)) s++; return (char *) s; }
 static inline char *skip_ws(const char *s)      { while (       isspace_c(*s)) s++; return (char *) s; }
 
-static inline char *depreciated_skip_back_non_ws(char *s) { while (s[-1] && !isspace_c(s[-1])) s--; return s; }
-static inline char *depreciated_skip_back_ws(char *s)     { while (isspace_c(s[-1]))           s--; return s; }
 
 static inline char *skip_back_non_ws(const char *s, const char *start) { while (s > start && !isspace_c(s[-1])) s--; return (char *) s; }
 static inline char *skip_back_ws(const char *s, const char *start)     { while (s > start && isspace_c(s[-1])) s--; return (char *) s; }
