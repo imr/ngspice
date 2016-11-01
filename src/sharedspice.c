bc0114bde47a97376fd6941eff76a3042a97f70f
h_vogt <h_vogt>
2016-11-01 21:26:29 +0100

    sharedspice.c, remove line create_circbyline(NULL);, breaks compatibility
    partial revert of
    inp.c, sharedspice.c, enable adding scripts by 'circbyline'

--- src/sharedspice.c
+++ src/sharedspice.c
@@ -844,7 +844,6 @@ int ngSpice_Circ(char** circa){
             newline = copy(circa[i]);
             create_circbyline(newline);
         }
-        create_circbyline(NULL);
         return 0;
     }
     /* upon error */
