59a4e4927409d83ff13539aeccfa71b1f05be8d1
h_vogt <h_vogt>
2016-11-29 00:18:23 +0100

    add rotation angle to text output, currently used only by postscript, ignored by all others

--- src/frontend/hpgl.c
+++ src/frontend/hpgl.c
@@ -243,9 +243,10 @@ GL_Arc(int x0, int y0, int r, double theta, double delta_theta)
 
 
 int
-GL_Text(char *text, int x, int y)
+GL_Text(char *text, int x, int y, int angle)
 {
     /* move to (x, y) */
+    NG_IGNORE(angle);
 
     fprintf(plotfile, "PU;PA %d , %d;", jgmult*(x+xoff+XTADJ), jgmult*(y+yoff+YTADJ));
     fprintf(plotfile, "LB %s \x03", text);
