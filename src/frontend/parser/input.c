d9e7ab27597bdc50eb86b1e9b206e2b84dfefaac
rlar <rlar>
2017-02-12 19:24:43 +0100

    input.c, simplify #ifdef for the broken visual studio _read()
    If we have _MSC_VER so we have either CONSOLE HAS_WINGUI or SHARED_MODULE
    If we have SHARED_MODULE, then we most certainly don't need
      the unbuffered read(), which isn't unbuffered on _MSC_VER anyway.
    Thus _MSC_VER is sufficient.

--- src/frontend/parser/input.c
+++ src/frontend/parser/input.c
@@ -25,7 +25,7 @@ int
 inchar(FILE *fp)
 {
 
-#if !(defined(HAS_WINGUI) || defined(_MSC_VER) && defined(CONSOLE))
+#if !(defined(HAS_WINGUI) || defined(_MSC_VER))
     if (cp_interactive && !cp_nocc) {
         char c;
         ssize_t i;
