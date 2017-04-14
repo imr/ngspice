494f1e1542c924af2299d4f1605bd25a793f7b79
rlar <rlar>
2017-04-14 11:37:10 +0200

    %.el, trailing whitespace and carriage return

--- src/spicelib/devices/bsim4v4/%.el
+++ src/spicelib/devices/bsim4v4/%.el
@@ -2,3 +2,6 @@
 
 (compile "unzip -j BSIM440.zip 'BSIM440/src/*'")
 (compile "git add b4*.c bsim4*.h inp2m.c inpdomod.c inpfindl.c noisean.c B4TERMS_OF_USE")
+
+;;; whitespace cleanup
+(compile "perl -i -pe 's/[ \\t\\r]*\\n/\\n/g;' *.c *.h")
