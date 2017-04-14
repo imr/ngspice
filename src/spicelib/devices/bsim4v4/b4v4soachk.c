42130cf44c6de6753a73495cc8a353cd9f9484e8
rlar <rlar>
2017-04-14 12:41:57 +0200

    execute first three

---
+++ src/spicelib/devices/bsim4v4/b4v4soachk.c
@@ -0,0 +1,341 @@
+/**********
+Copyright 2013 Dietmar Warning. All rights reserved.
+Author:   2013 Dietmar Warning
+**********/
+
+#include "ngspice/ngspice.h"
+#include "ngspice/cktdefs.h"
+#include "bsim4v5def.h"
+#include "ngspice/trandefs.h"
+#include "ngspice/sperror.h"
+#include "ngspice/suffix.h"
+#include "ngspice/cpdefs.h"
+
+
+int
+BSIM4v5soaCheck(CKTcircuit *ckt, GENmodel *inModel)
+{
+    BSIM4v5model *model = (BSIM4v5model *) inModel;
+    BSIM4v5instance *here;
+    double vgs, vgd, vgb, vds, vbs, vbd;    /* actual mos voltages */
+    int maxwarns;
+    static int warns_vgs = 0, warns_vgd = 0, warns_vgb = 0, warns_vds = 0, warns_vbs = 0, warns_vbd = 0;
+
+    if (!ckt) {
+        warns_vgs = 0;
+        warns_vgd = 0;
+        warns_vgb = 0;
+        warns_vds = 0;
+        warns_vbs = 0;
+        warns_vbd = 0;
+        return OK;
+    }
+
+    maxwarns = ckt->CKTsoaMaxWarns;
+
+    for (; model; model = model->BSIM4v5nextModel) {
+
+        for (here = model->BSIM4v5instances; here; here = here->BSIM4v5nextInstance) {
+
+            vgs = ckt->CKTrhsOld [here->BSIM4v5gNodePrime] -
+                  ckt->CKTrhsOld [here->BSIM4v5sNodePrime];
+
+            vgd = ckt->CKTrhsOld [here->BSIM4v5gNodePrime] -
+                  ckt->CKTrhsOld [here->BSIM4v5dNodePrime];
+
+            vgb = ckt->CKTrhsOld [here->BSIM4v5gNodePrime] -
+                  ckt->CKTrhsOld [here->BSIM4v5bNodePrime];
+
+            vds = ckt->CKTrhsOld [here->BSIM4v5dNodePrime] -
+                  ckt->CKTrhsOld [here->BSIM4v5sNodePrime];
+
+            vbs = ckt->CKTrhsOld [here->BSIM4v5bNodePrime] -
+                  ckt->CKTrhsOld [here->BSIM4v5sNodePrime];
+
+            vbd = ckt->CKTrhsOld [here->BSIM4v5bNodePrime] -
+                  ckt->CKTrhsOld [here->BSIM4v5dNodePrime];
+
+            if (!model->BSIM4v5vgsrMaxGiven) {
+                if (fabs(vgs) > model->BSIM4v5vgsMax)
+                    if (warns_vgs < maxwarns) {
+                        soa_printf(ckt, (GENinstance*) here,
+                                   "Vgs=%g has exceeded Vgs_max=%g\n",
+                                   vgs, model->BSIM4v5vgsMax);
+                        warns_vgs++;
+                    }
+                if (!model->BSIM4v5vgbMaxGiven) {
+                    if (fabs(vgb) > model->BSIM4v5vgsMax)
+                        if (warns_vgb < maxwarns) {
+                            soa_printf(ckt, (GENinstance*) here,
+                                       "Vgb=%g has exceeded Vgs_max=%g\n",
+                                       vgb, model->BSIM4v5vgsMax);
+                            warns_vgb++;
+                        }
+                } else {
+                    if (fabs(vgb) > model->BSIM4v5vgbMax)
+                        if (warns_vgb < maxwarns) {
+                            soa_printf(ckt, (GENinstance*) here,
+                                       "Vgb=%g has exceeded Vgb_max=%g\n",
+                                       vgb, model->BSIM4v5vgbMax);
+                            warns_vgb++;
+                        }
+                }
+            } else {
+                if (model->BSIM4v5type > 0) {
+                    if (vgs > model->BSIM4v5vgsMax)
+                        if (warns_vgs < maxwarns) {
+                            soa_printf(ckt, (GENinstance*) here,
+                                       "Vgs=%g has exceeded Vgs_max=%g\n",
+                                       vgs, model->BSIM4v5vgsMax);
+                            warns_vgs++;
+                        }
+                    if (-1*vgs > model->BSIM4v5vgsrMax)
+                        if (warns_vgs < maxwarns) {
+                            soa_printf(ckt, (GENinstance*) here,
+                                       "Vgs=%g has exceeded Vgsr_max=%g\n",
+                                       vgs, model->BSIM4v5vgsrMax);
+                            warns_vgs++;
+                        }
+                } else {
+                    if (vgs > model->BSIM4v5vgsrMax)
+                        if (warns_vgs < maxwarns) {
+                            soa_printf(ckt, (GENinstance*) here,
+                                       "Vgs=%g has exceeded Vgsr_max=%g\n",
+                                       vgs, model->BSIM4v5vgsrMax);
+                            warns_vgs++;
+                        }
+                    if (-1*vgs > model->BSIM4v5vgsMax)
+                        if (warns_vgs < maxwarns) {
+                            soa_printf(ckt, (GENinstance*) here,
+                                       "Vgs=%g has exceeded Vgs_max=%g\n",
+                                       vgs, model->BSIM4v5vgsMax);
+                            warns_vgs++;
+                        }
+                }
+            }
+
+            if (!model->BSIM4v5vgdrMaxGiven) {
+                if (fabs(vgd) > model->BSIM4v5vgdMax)
+                    if (warns_vgd < maxwarns) {
+                        soa_printf(ckt, (GENinstance*) here,
+                                   "Vgd=%g has exceeded Vgd_max=%g\n",
+                                   vgd, model->BSIM4v5vgdMax);
+                        warns_vgd++;
+                    }
+            } else {
+                if (model->BSIM4v5type > 0) {
+                    if (vgd > model->BSIM4v5vgdMax)
+                        if (warns_vgd < maxwarns) {
+                            soa_printf(ckt, (GENinstance*) here,
+                                       "Vgd=%g has exceeded Vgd_max=%g\n",
+                                       vgd, model->BSIM4v5vgdMax);
+                            warns_vgd++;
+                        }
+                    if (-1*vgd > model->BSIM4v5vgdrMax)
+                        if (warns_vgd < maxwarns) {
+                            soa_printf(ckt, (GENinstance*) here,
+                                       "Vgd=%g has exceeded Vgdr_max=%g\n",
+                                       vgd, model->BSIM4v5vgdrMax);
+                            warns_vgd++;
+                        }
+                } else {
+                    if (vgd > model->BSIM4v5vgdrMax)
+                        if (warns_vgd < maxwarns) {
+                            soa_printf(ckt, (GENinstance*) here,
+                                       "Vgd=%g has exceeded Vgdr_max=%g\n",
+                                       vgd, model->BSIM4v5vgdrMax);
+                            warns_vgd++;
+                        }
+                    if (-1*vgd > model->BSIM4v5vgdMax)
+                        if (warns_vgd < maxwarns) {
+                            soa_printf(ckt, (GENinstance*) here,
+                                       "Vgd=%g has exceeded Vgd_max=%g\n",
+                                       vgd, model->BSIM4v5vgdMax);
+                            warns_vgd++;
+                        }
+                }
+            }
+
+            if (fabs(vds) > model->BSIM4v5vdsMax)
+                if (warns_vds < maxwarns) {
+                    soa_printf(ckt, (GENinstance*) here,
+                               "Vds=%g has exceeded Vds_max=%g\n",
+                               vds, model->BSIM4v5vdsMax);
+                    warns_vds++;
+                }
+
+            if (!model->BSIM4v5vgbrMaxGiven) {
+                if (fabs(vgb) > model->BSIM4v5vgbMax)
+                    if (warns_vgb < maxwarns) {
+                        soa_printf(ckt, (GENinstance*) here,
+                                   "Vgb=%g has exceeded Vgb_max=%g\n",
+                                   vgb, model->BSIM4v5vgbMax);
+                        warns_vgb++;
+                    }
+            } else {
+                if (model->BSIM4v5type > 0) {
+                    if (vgb > model->BSIM4v5vgbMax)
+                        if (warns_vgb < maxwarns) {
+                            soa_printf(ckt, (GENinstance*) here,
+                                       "Vgb=%g has exceeded Vgb_max=%g\n",
+                                       vgb, model->BSIM4v5vgbMax);
+                            warns_vgb++;
+                        }
+                    if (-1*vgb > model->BSIM4v5vgbrMax)
+                        if (warns_vgb < maxwarns) {
+                            soa_printf(ckt, (GENinstance*) here,
+                                       "Vgb=%g has exceeded Vgbr_max=%g\n",
+                                       vgb, model->BSIM4v5vgbrMax);
+                            warns_vgb++;
+                        }
+                } else {
+                    if (vgb > model->BSIM4v5vgbrMax)
+                        if (warns_vgb < maxwarns) {
+                            soa_printf(ckt, (GENinstance*) here,
+                                       "Vgb=%g has exceeded Vgbr_max=%g\n",
+                                       vgb, model->BSIM4v5vgbrMax);
+                            warns_vgb++;
+                        }
+                    if (-1*vgb > model->BSIM4v5vgbMax)
+                        if (warns_vgb < maxwarns) {
+                            soa_printf(ckt, (GENinstance*) here,
+                                       "Vgb=%g has exceeded Vgb_max=%g\n",
+                                       vgb, model->BSIM4v5vgbMax);
+                            warns_vgb++;
+                        }
+                }
+            }
+
+            if (!model->BSIM4v5vbsrMaxGiven) {
+                if (!model->BSIM4v5vbsMaxGiven) {
+                    if (fabs(vbs) > model->BSIM4v5vbdMax)
+                        if (warns_vbs < maxwarns) {
+                            soa_printf(ckt, (GENinstance*) here,
+                                       "Vbs=%g has exceeded Vbd_max=%g\n",
+                                       vbs, model->BSIM4v5vbdMax);
+                            warns_vbs++;
+                        }
+                } else {
+                    if (fabs(vbs) > model->BSIM4v5vbsMax)
+                        if (warns_vbs < maxwarns) {
+                            soa_printf(ckt, (GENinstance*) here,
+                                       "Vbs=%g has exceeded Vbs_max=%g\n",
+                                       vbs, model->BSIM4v5vbsMax);
+                            warns_vbs++;
+                        }
+                }
+            } else {
+                if (!model->BSIM4v5vbsMaxGiven) {
+                    if (model->BSIM4v5type > 0) {
+                        if (vbs > model->BSIM4v5vbdMax)
+                            if (warns_vbs < maxwarns) {
+                                soa_printf(ckt, (GENinstance*) here,
+                                           "Vbs=%g has exceeded Vbd_max=%g\n",
+                                           vbs, model->BSIM4v5vbdMax);
+                                warns_vbs++;
+                            }
+                        if (-1*vbs > model->BSIM4v5vbsrMax)
+                            if (warns_vbs < maxwarns) {
+                                soa_printf(ckt, (GENinstance*) here,
+                                           "Vbs=%g has exceeded Vbsr_max=%g\n",
+                                           vbs, model->BSIM4v5vbsrMax);
+                                warns_vbs++;
+                            }
+                    } else {
+                        if (vbs > model->BSIM4v5vbsrMax)
+                            if (warns_vbs < maxwarns) {
+                                soa_printf(ckt, (GENinstance*) here,
+                                           "Vbs=%g has exceeded Vbsr_max=%g\n",
+                                           vbs, model->BSIM4v5vbsrMax);
+                                warns_vbs++;
+                            }
+                        if (-1*vbs > model->BSIM4v5vbdMax)
+                            if (warns_vbs < maxwarns) {
+                                soa_printf(ckt, (GENinstance*) here,
+                                           "Vbs=%g has exceeded Vbd_max=%g\n",
+                                           vbs, model->BSIM4v5vbdMax);
+                                warns_vbs++;
+                            }
+                    }
+                } else {
+                    if (model->BSIM4v5type > 0) {
+                        if (vbs > model->BSIM4v5vbsMax)
+                            if (warns_vbs < maxwarns) {
+                                soa_printf(ckt, (GENinstance*) here,
+                                           "Vbs=%g has exceeded Vbs_max=%g\n",
+                                           vbs, model->BSIM4v5vbsMax);
+                                warns_vbs++;
+                            }
+                        if (-1*vbs > model->BSIM4v5vbsrMax)
+                            if (warns_vbs < maxwarns) {
+                                soa_printf(ckt, (GENinstance*) here,
+                                           "Vbs=%g has exceeded Vbsr_max=%g\n",
+                                           vbs, model->BSIM4v5vbsrMax);
+                                warns_vbs++;
+                            }
+                    } else {
+                        if (vbs > model->BSIM4v5vbsrMax)
+                            if (warns_vbs < maxwarns) {
+                                soa_printf(ckt, (GENinstance*) here,
+                                           "Vbs=%g has exceeded Vbsr_max=%g\n",
+                                           vbs, model->BSIM4v5vbsrMax);
+                                warns_vbs++;
+                            }
+                        if (-1*vbs > model->BSIM4v5vbsMax)
+                            if (warns_vbs < maxwarns) {
+                                soa_printf(ckt, (GENinstance*) here,
+                                           "Vbs=%g has exceeded Vbs_max=%g\n",
+                                           vbs, model->BSIM4v5vbsMax);
+                                warns_vbs++;
+                            }
+                    }
+                }
+            }
+
+            if (!model->BSIM4v5vbdrMaxGiven) {
+                if (fabs(vbd) > model->BSIM4v5vbdMax)
+                    if (warns_vbd < maxwarns) {
+                        soa_printf(ckt, (GENinstance*) here,
+                                   "Vbd=%g has exceeded Vbd_max=%g\n",
+                                   vbd, model->BSIM4v5vbdMax);
+                        warns_vbd++;
+                    }
+            } else {
+                if (model->BSIM4v5type > 0) {
+                    if (vbd > model->BSIM4v5vbdMax)
+                        if (warns_vbd < maxwarns) {
+                            soa_printf(ckt, (GENinstance*) here,
+                                       "Vbd=%g has exceeded Vbd_max=%g\n",
+                                       vbd, model->BSIM4v5vbdMax);
+                            warns_vbd++;
+                        }
+                    if (-1*vbd > model->BSIM4v5vbdrMax)
+                        if (warns_vbd < maxwarns) {
+                            soa_printf(ckt, (GENinstance*) here,
+                                       "Vbd=%g has exceeded Vbdr_max=%g\n",
+                                       vbd, model->BSIM4v5vbdrMax);
+                            warns_vbd++;
+                        }
+                } else {
+                    if (vbd > model->BSIM4v5vbdrMax)
+                        if (warns_vbd < maxwarns) {
+                            soa_printf(ckt, (GENinstance*) here,
+                                       "Vbd=%g has exceeded Vbdr_max=%g\n",
+                                       vbd, model->BSIM4v5vbdrMax);
+                            warns_vbd++;
+                        }
+                    if (-1*vbd > model->BSIM4v5vbdMax)
+                        if (warns_vbd < maxwarns) {
+                            soa_printf(ckt, (GENinstance*) here,
+                                       "Vbd=%g has exceeded Vbd_max=%g\n",
+                                       vbd, model->BSIM4v5vbdMax);
+                            warns_vbd++;
+                        }
+                }
+            }
+
+        }
+    }
+
+    return OK;
+}
---
+++ src/spicelib/devices/bsim4v4/bsim4v4init.c
@@ -0,0 +1,84 @@
+#include "ngspice/config.h"
+
+#include "ngspice/devdefs.h"
+
+#include "bsim4v5itf.h"
+#include "bsim4v5ext.h"
+#include "bsim4v5init.h"
+
+
+SPICEdev BSIM4v5info = {
+    {
+        "BSIM4v5",
+        "Berkeley Short Channel IGFET Model-4",
+
+        &BSIM4v5nSize,
+        &BSIM4v5nSize,
+        BSIM4v5names,
+
+        &BSIM4v5pTSize,
+        BSIM4v5pTable,
+
+        &BSIM4v5mPTSize,
+        BSIM4v5mPTable,
+
+#ifdef XSPICE
+/*----  Fixed by SDB 5.2.2003 to enable XSPICE/tclspice integration  -----*/
+        NULL,  /* This is a SPICE device, it has no MIF info data */
+
+        0,     /* This is a SPICE device, it has no MIF info data */
+        NULL,  /* This is a SPICE device, it has no MIF info data */
+
+        0,     /* This is a SPICE device, it has no MIF info data */
+        NULL,  /* This is a SPICE device, it has no MIF info data */
+
+        0,     /* This is a SPICE device, it has no MIF info data */
+        NULL,  /* This is a SPICE device, it has no MIF info data */
+/*---------------------------  End of SDB fix   -------------------------*/
+#endif
+
+        DEV_DEFAULT
+    },
+
+    BSIM4v5param,    /* DEVparam       */
+    BSIM4v5mParam,   /* DEVmodParam    */
+    BSIM4v5load,     /* DEVload        */
+    BSIM4v5setup,    /* DEVsetup       */
+    BSIM4v5unsetup,  /* DEVunsetup     */
+    BSIM4v5setup,    /* DEVpzSetup     */
+    BSIM4v5temp,     /* DEVtemperature */
+    BSIM4v5trunc,    /* DEVtrunc       */
+    NULL,          /* DEVfindBranch  */
+    BSIM4v5acLoad,   /* DEVacLoad      */
+    NULL,          /* DEVaccept      */
+    BSIM4v5destroy,  /* DEVdestroy     */
+    BSIM4v5mDelete,  /* DEVmodDelete   */
+    BSIM4v5delete,   /* DEVdelete      */
+    BSIM4v5getic,    /* DEVsetic       */
+    BSIM4v5ask,      /* DEVask         */
+    BSIM4v5mAsk,     /* DEVmodAsk      */
+    BSIM4v5pzLoad,   /* DEVpzLoad      */
+    BSIM4v5convTest, /* DEVconvTest    */
+    NULL,          /* DEVsenSetup    */
+    NULL,          /* DEVsenLoad     */
+    NULL,          /* DEVsenUpdate   */
+    NULL,          /* DEVsenAcLoad   */
+    NULL,          /* DEVsenPrint    */
+    NULL,          /* DEVsenTrunc    */
+    NULL,          /* DEVdisto       */
+    BSIM4v5noise,    /* DEVnoise       */
+    BSIM4v5soaCheck, /* DEVsoaCheck    */
+#ifdef CIDER
+    NULL,          /* DEVdump        */
+    NULL,          /* DEVacct        */
+#endif
+    &BSIM4v5iSize,   /* DEVinstSize    */
+    &BSIM4v5mSize    /* DEVmodSize     */
+};
+
+
+SPICEdev *
+get_bsim4v5_info(void)
+{
+    return &BSIM4v5info;
+}
