634b01249247e7fda147c623c7d6347ccaae4087
rlar <rlar>
2017-04-20 17:41:13 +0200

    inpcom.c, inp_chk_for_multi_in_vcvs(), #3/6, xx_new_line() shall fill the li_next slot

--- src/frontend/inpcom.c
+++ src/frontend/inpcom.c
@@ -1354,9 +1354,8 @@ inp_chk_for_multi_in_vcvs(struct line *c, int *line_number)
                 next_card   = c->li_next;
                 a_card = xx_new_line(NULL, m_instance, (*line_number)++, 0);
                 c->li_next  = a_card;
-                model_card = xx_new_line(NULL, m_model, (*line_number)++, 0);
+                model_card = xx_new_line(next_card, m_model, (*line_number)++, 0);
                 a_card->li_next     = model_card;
-                model_card->li_next = next_card;
                 // skip these two new cards
                 c = model_card;
             }
