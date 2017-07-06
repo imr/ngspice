ebf0b8ce6824945d9e5d9e3083b7bc4ef869d45b
h_vogt <h_vogt>
2017-07-06 11:14:31 +0200

    com_plot.c, tabs -> spaces

--- src/frontend/com_plot.c
+++ src/frontend/com_plot.c
@@ -28,15 +28,15 @@ com_bltplot(wordlist *wl)
 void
 com_clip(wordlist *wl)
 {
-	int err;
-	double xmin, xmax;
-	char *vecname = wl->wl_word;
-	wl = wl->wl_next;
-	char *mword = wl->wl_word;
-	xmin = INPevaluate(&mword, &err, TRUE);
-	wl = wl->wl_next;
-	mword = wl->wl_word;
-	xmax = INPevaluate(&mword, &err, TRUE);
-	vec_clip(vecname, xmin, xmax);
+    int err;
+    double xmin, xmax;
+    char *vecname = wl->wl_word;
+    wl = wl->wl_next;
+    char *mword = wl->wl_word;
+    xmin = INPevaluate(&mword, &err, TRUE);
+    wl = wl->wl_next;
+    mword = wl->wl_word;
+    xmax = INPevaluate(&mword, &err, TRUE);
+    vec_clip(vecname, xmin, xmax);
 }
 
