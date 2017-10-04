ab5abf6fcdb615cfce494686ad52e1766ef7e377
h_vogt <h_vogt>
2017-10-04 17:19:23 +0200

    add command setcf

--- src/frontend/commands.c
+++ src/frontend/commands.c
@@ -126,7 +126,10 @@ struct comm spcp_coms[] = {
       { 020000, 020000, 020000, 020000 }, E_DEFHMASK, 0, LOTS,
       arg_set,
       "[option] [option = value] ... : Set a variable." } ,
-
+    { "setcf", com_set, FALSE, TRUE,
+      { 020000, 020000, 020000, 020000 }, E_DEFHMASK, 0, LOTS,
+      arg_set,
+      "[option] [option = value] ... : Set a variable, case remains as given." } ,
 
 /* support for altering options in interactive mode,
    using either command 'option' or 'options'*/
@@ -617,7 +620,10 @@ struct comm nutcp_coms[] = {
       { 020000, 020000, 020000, 020000 }, E_DEFHMASK, 0, LOTS,
       arg_set,
       "[option] [option = value] ... : Set a variable." } ,
-
+    { "setcf", com_set, FALSE, TRUE,
+      { 020000, 020000, 020000, 020000 }, E_DEFHMASK, 0, LOTS,
+      arg_set,
+      "[option] [option = value] ... : Set a variable, case remains as given." } ,
 #ifdef EXPERIMENTAL_CODE
 /* PN support for altering options in interactive mode */
     { "option", com_option, TRUE, TRUE,
