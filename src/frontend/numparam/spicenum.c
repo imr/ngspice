5bbd25e2da47025199ccf74f691fed3de18be3b4
rlar <rlar>
2017-10-29 20:07:15 +0100

    numparam/xpressn.c, #5/5, drop unused assignment

--- src/frontend/numparam/spicenum.c
+++ src/frontend/numparam/spicenum.c
@@ -788,7 +788,6 @@ nupa_copy(char *s, int linenum)
         dicoS->dyncategory[linenum] = c;
     } /* keep a local copy and mangle the string */
 
-    ls = spice_dstring_length(&u);
     t = strdup(spice_dstring_value(&u));
 
     if (t == NULL) {
