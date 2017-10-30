981fed70ca69d4dc68ac9f9d3fdb98fd81058ec3
rlar <rlar>
2017-10-30 16:31:41 +0100

    struct card, #2/6, drop struct line
    struct line is an overlay of struct card.

--- src/include/ngspice/fteinp.h
+++ src/include/ngspice/fteinp.h
@@ -3,25 +3,10 @@ Copyright 1990 Regents of the University of California.  All rights reserved.
 Author: 1985 Wayne A. Christopher, U. C. Berkeley CAD Group
 **********/
 
-/*
- *
- * Note that this definition must be the same as struct card in INPdefs.h...
- */
-
 #ifndef ngspice_FTEINP_H
 #define ngspice_FTEINP_H
 
 #include "ngspice/inpdefs.h"
-
-/* This struct defines a linked list of lines from a SPICE file. */
-struct line {
-    int li_linenum;
-    int li_linenum_orig;
-    char *li_line;
-    char *li_error;
-    struct line *li_next;
-    struct line *li_actual;
-} ;
 
 /* Listing types. */
 
