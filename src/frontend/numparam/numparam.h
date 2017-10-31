6a1c9f934cdba706c6aca9db1943d8d98a02b67a
rlar <rlar>
2017-10-31 10:45:48 +0100

    numparam, entry_type, #1/5, introduce typedef 'nupa_type'

--- src/frontend/numparam/numparam.h
+++ src/frontend/numparam/numparam.h
@@ -19,8 +19,19 @@ typedef enum {Psp     = '{'} _nPsp;      /* Ps expression */
  * I believe the entry_t should be a union of type but I need more info.
  * ----------------------------------------------------------------- */
 
+#define  NUPA_REAL     'R'
+#define  NUPA_STRING   'S'
+#define  NUPA_POINTER  'P'
+#define  NUPA_SUBCKT   'U'
+#define  NUPA_SPACE    ' '
+#define  NUPA_UNKNOWN  '?'
+#define  NUPA_MODEL    'O'
+
+typedef char nupa_type;
+
+
 typedef struct entry_s {
-    char   tp;         /* type: I)nt R)eal S)tring F)unction M)acro P)ointer */
+    nupa_type tp;      /* type: I)nt R)eal S)tring F)unction M)acro P)ointer */
     char *symbol;
     int  level;                 /* subckt nesting level */
     double vl;                  /* float value if defined */
@@ -52,13 +63,13 @@ typedef struct {                /* the input scanner data structure */
 void initdico(dico_t *);
 int donedico(dico_t *);
 void dico_free_entry(entry_t *);
-bool defsubckt(dico_t *, struct card *, char categ);
+bool defsubckt(dico_t *, struct card *, nupa_type categ);
 int findsubckt(dico_t *, char *s, SPICE_DSTRINGPTR subname);
 bool nupa_substitute(dico_t *, char *s, char *r, bool err);
 bool nupa_assignment(dico_t *, char *s, char mode);
 bool nupa_subcktcall(dico_t *, char *s, char *x, bool err);
 void nupa_subcktexit(dico_t *);
 dico_t *nupa_fetchinstance(void);
-char getidtype(dico_t *, char *s);
+nupa_type getidtype(dico_t *, char *s);
 entry_t *attrib(dico_t *, NGHASHPTR htable, char *t, char op);
 void del_attrib(void *);
