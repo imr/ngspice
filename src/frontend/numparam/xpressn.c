f435bb6552b06cd2cda91b66934e82e8d989141f
rlar <rlar>
2017-11-18 12:13:47 +0100

    getexpress(), fix lvalues

--- src/frontend/numparam/xpressn.c
+++ src/frontend/numparam/xpressn.c
@@ -1371,14 +1371,14 @@ getexpress(const char * const s, SPICE_DSTRINGPTR tstr_p, const char **pi)
     nupa_type tpe;
 
     ls_ptr = s + strlen(s);
-    (xia_ptr + 1) = (p + 1) + 1;
+    xia_ptr = (p + 1) + 1 - 1;
 
     while (((xia_ptr + 1) < ls_ptr) && ((xia_ptr + 1)[-1] <= ' '))
-        (xia_ptr + 1)++;                   /*white space ? */
+        xia_ptr++;              /*white space ? */
 
     if ((xia_ptr + 1)[-1] == '"') {
         /* string constant */
-        (xia_ptr + 1)++;
+        xia_ptr++;
         p = (xia_ptr + 1) - 1;
 
         while (((p + 1) < ls_ptr) && (*p != '"'))
@@ -1393,7 +1393,7 @@ getexpress(const char * const s, SPICE_DSTRINGPTR tstr_p, const char **pi)
     } else {
 
         if ((xia_ptr + 1)[-1] == '{')
-            (xia_ptr + 1)++;
+            xia_ptr++;
 
         p = (xia_ptr + 1) - 1 - 1;
 
