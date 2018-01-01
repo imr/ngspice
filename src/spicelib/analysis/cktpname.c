c3a26e3cd2716f61731eba3bc74b40a3d19cbcf5
rlar <rlar>
2018-01-01 15:09:44 +0100

    analysis/cktpname.c, cleanup, mostly whitespace

--- src/spicelib/analysis/cktpname.c
+++ src/spicelib/analysis/cktpname.c
@@ -2,14 +2,12 @@
 Copyright 1990 Regents of the University of California.  All rights reserved.
 Author: 1985 Thomas L. Quarles
 **********/
-/*
- */
 
-    /*
-     * CKTpName()
-     *
-     *  Take a parameter by Name and set it on the specified device 
-     */
+/*
+ * CKTpName()
+ *
+ *  Take a parameter by Name and set it on the specified device
+ */
 
 #include "ngspice/ngspice.h"
 #include "ngspice/ifsim.h"
@@ -19,33 +17,31 @@ Author: 1985 Thomas L. Quarles
 #include "ngspice/sperror.h"
 
 
-/* ARGSUSED */
+/* the name of the parameter to set */
+/* the parameter union containing the value to set */
+/* the circuit this device is a member of */
+/* the device type code to the device being parsed */
+/* the name of the device being parsed */
+/* direct pointer to device being parsed */
+
 int
 CKTpName(char *parm, IFvalue *val, CKTcircuit *ckt, int dev, char *name, GENinstance **fast)
-                /* the name of the parameter to set */
-                  /* the parameter union containing the value to set */
-                    /* the circuit this device is a member of */
-                    /* the device type code to the device being parsed */
-                    /* the name of the device being parsed */
-                           /* direct pointer to device being parsed */
-
 {
-    int error;  /* int to store evaluate error return codes in */
     int i;
 
     NG_IGNORE(name);
 
-    for(i = 0 ; i < *(DEVices[dev]->DEVpublic.numInstanceParms) ; i++) {
-        if(!strcmp(parm, DEVices[dev]->DEVpublic.instanceParms[i].keyword)) {
-            error = CKTparam(ckt, *fast,
-                    DEVices[dev]->DEVpublic.instanceParms[i].id, val,
-                    NULL);
-            if(error) return(error);
+    for (i = 0; i < *(DEVices[dev]->DEVpublic.numInstanceParms); i++)
+        if (!strcmp(parm, DEVices[dev]->DEVpublic.instanceParms[i].keyword)) {
+            int error = CKTparam(ckt, *fast,
+                                 DEVices[dev]->DEVpublic.instanceParms[i].id, val, NULL);
+            if (error)
+                return error;
             break;
         }
-    }
-    if(i == *(DEVices[dev]->DEVpublic.numInstanceParms)) {
-        return(E_BADPARM);
-    }
-    return(OK);
+
+    if (i == *(DEVices[dev]->DEVpublic.numInstanceParms))
+        return E_BADPARM;
+
+    return OK;
 }
