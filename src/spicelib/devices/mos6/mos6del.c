80d5614f49d60b21a44ebbaa59c4429a3a04f408
rlar <rlar>
2018-01-14 12:47:31 +0100

    devices/os6, introduce missing files mos6del.c and  mos6mdel.c
    For unknown reason, mos6 is missing
      MOS6mDelete() aka DEVmodDelete()
    and
      MOS6delete() aka DEVdelete()

---
+++ src/spicelib/devices/mos6/mos6del.c
@@ -0,0 +1,29 @@
+/**********
+Copyright 1990 Regents of the University of California.  All rights reserved.
+Author: 1985 Thomas L. Quarles
+**********/
+
+#include "ngspice/ngspice.h"
+#include "mos6defs.h"
+#include "ngspice/sperror.h"
+#include "ngspice/suffix.h"
+
+
+int
+MOS6delete(GENmodel *model, IFuid name, GENinstance **kill)
+{
+    for (; model; model = model->GENnextModel) {
+        GENinstance **prev = &(model->GENinstances);
+        GENinstance *here = *prev;
+        for (; here; here = *prev) {
+            if (here->GENname == name || (kill && here == *kill)) {
+                *prev = here->GENnextInstance;
+                GENinstanceFree(here);
+                return OK;
+            }
+            prev = &(here->GENnextInstance);
+        }
+    }
+
+    return E_NODEV;
+}
---
+++ src/spicelib/devices/mos6/mos6mdel.c
@@ -0,0 +1,36 @@
+/**********
+Copyright 1990 Regents of the University of California.  All rights reserved.
+Author: 1985 Thomas L. Quarles
+**********/
+
+#include "ngspice/ngspice.h"
+#include "mos6defs.h"
+#include "ngspice/sperror.h"
+#include "ngspice/suffix.h"
+
+
+int
+MOS6mDelete(GENmodel **models, IFuid modname, GENmodel *kill)
+{
+    GENinstance *here;
+    GENmodel **prev = models;
+    GENmodel *model = *prev;
+
+    for (; model; model = model->GENnextModel) {
+        if (model->GENmodName == modname || (kill && model == kill))
+            break;
+        prev = &(model->GENnextModel);
+    }
+
+    if (!model)
+        return E_NOMOD;
+
+    *prev = model->GENnextModel;
+    for (here = model->GENinstances; here;) {
+        GENinstance *next_instance = here->GENnextInstance;
+        GENinstanceFree(here);
+        here = next_instance;
+    }
+    GENmodelFree(model);
+    return OK;
+}
