c892b2d460159befd195647175d6f41fdc9c87f9
rlar <rlar>
2018-01-18 18:01:25 +0100

    analysis/acan.c, check for valid startfreq
    an 'ac dec' or 'ac oct' can not have a startfreq <= 0

--- src/spicelib/analysis/acan.c
+++ src/spicelib/analysis/acan.c
@@ -82,10 +82,18 @@ ACan(CKTcircuit *ckt, int restart)
         switch (job->ACstepType) {
 
         case DECADE:
+            if (job->ACstartFreq <= 0) {
+                fprintf(stderr, "ERROR: AC startfreq <= 0\n");
+                return E_PARMVAL;
+            }
             job->ACfreqDelta =
                 exp(log(10.0)/job->ACnumberSteps);
             break;
         case OCTAVE:
+            if (job->ACstartFreq <= 0) {
+                fprintf(stderr, "ERROR: AC startfreq <= 0\n");
+                return E_PARMVAL;
+            }
             job->ACfreqDelta =
                 exp(log(2.0)/job->ACnumberSteps);
             break;
