ca391753b0af9b3b59b31967a12fb86e684fafc9
rlar <rlar>
2018-03-18 14:44:33 +0100

    remove bsimcmg completely

--- src/spicelib/parser/inp2m.c
+++ src/spicelib/parser/inp2m.c
@@ -32,8 +32,7 @@ model_numnodes(int type)
     }
 
 #ifdef ADMS
-    if (type == INPtypelook("BSIMBULK") ||  /* bsimbulk.va */
-        type == INPtypelook("BSIMCMG"))     /* bsimcmg.va */
+    if (type == INPtypelook("BSIMBULK"))    /* bsimbulk.va */
     {
         return 5;
     }
@@ -134,7 +133,6 @@ INP2M(CKTcircuit *ckt, INPtables *tab, card *current)
         thismodel->INPmodType != INPtypelook("ekv") &&
         thismodel->INPmodType != INPtypelook("psp102") &&
         thismodel->INPmodType != INPtypelook("bsimbulk") &&
-        thismodel->INPmodType != INPtypelook("bsimcmg") &&
 #endif
         thismodel->INPmodType != INPtypelook("HiSIM2") &&
         thismodel->INPmodType != INPtypelook("HiSIMHV1") &&
