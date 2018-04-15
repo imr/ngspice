a92c11b295cdc6c0835c2b80be7b2a6c5b1c6923
rlar <rlar>
2018-04-15 08:42:02 +0200

    devices/vbic/vbicsetup.c, bug fix, incorrect states allocation
    VBICnumSenStates is 10 not 8

--- src/spicelib/devices/vbic/vbicsetup.c
+++ src/spicelib/devices/vbic/vbicsetup.c
@@ -403,7 +403,7 @@ VBICsetup(SMPmatrix *matrix, GENmodel *inModel, CKTcircuit *ckt, int *states)
             here->VBICstate = *states;
             *states += VBICnumStates;
             if(ckt->CKTsenInfo && (ckt->CKTsenInfo->SENmode & TRANSEN) ){
-                *states += 8 * (ckt->CKTsenInfo->SENparms);
+                *states += 10 * (ckt->CKTsenInfo->SENparms);
             }
 
             if(model->VBICextCollResist == 0) {
