21ef095b4ef7035c524973b50a166e91eb79dd14
Holger Vogt <holger.vogt@uni-due.de>
2019-01-27 22:33:24 +0100

    Revert "take the plot data vector from the node selected - not the current plot"
    This reverts commit 125600db9f3762b47c8a047bacbbb984dbd3801e.

--- src/frontend/evaluate.c
+++ src/frontend/evaluate.c
@@ -878,13 +878,7 @@ apply_func(struct func *func, struct pnode *arg)
             fprintf(cp_err, "Error: bad v() syntax\n");
             return (NULL);
         }
-        /* function 'v': take the plot data vector from the node selected - not the current plot */
-        if (func->fu_name && eq(func->fu_name, "v")) {
-           t = arg->pn_value;
-        }
-        else {
-            t = vec_fromplot(arg->pn_value->v_name, plot_cur);
-        }
+        t = vec_fromplot(arg->pn_value->v_name, get_plot(arg->pn_value->v_plot->pl_typename));
         if (!t) {
             fprintf(cp_err, "Error: no such vector %s\n", arg->pn_value->v_name);
             return (NULL);
