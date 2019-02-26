54d4d9747a1e22396982f31cad9a11d72f9bd9db
Holger Vogt <holger.vogt@uni-due.de>
2019-02-26 20:20:17 +0100

    ngspice version is now 30+

--- visualc/src/include/ngspice/config.h
+++ visualc/src/include/ngspice/config.h
@@ -455,13 +455,13 @@
 #define PACKAGE_NAME "ngspice"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "ngspice 30"
+#define PACKAGE_STRING "ngspice 30+"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "ngspice"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "30"
+#define PACKAGE_VERSION "30+"
 
 /* Define if we want predictor algorithm */
 /* #undef PREDICTOR */
@@ -516,7 +516,7 @@
 /* #undef TM_IN_SYS_TIME */
 
 /* Version number of package */
-#define VERSION "30"
+#define VERSION "30+"
 
 /* Define if we want spice2 sensitivity analysis */
 /* #undef WANT_SENSE2 */
