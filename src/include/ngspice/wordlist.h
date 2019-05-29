1c5176282b8c26b0eb37f025b0e8148438e96e16
Jim Monte <jimmonte@users.sourceforge.net>
2019-05-29 22:17:53 +0200

    [PATCH #52] General cleanup of word list processing, such as adding  comments and const. Also added a new function wl_from_string() to convert a  single word into a word list. This function can be useful when calling a  function taking a single-word word list.

--- src/include/ngspice/wordlist.h
+++ src/include/ngspice/wordlist.h
@@ -11,28 +11,29 @@ struct wordlist {
 
 typedef struct wordlist wordlist;
 
-int wl_length(const wordlist *wlist);
-void wl_free(wordlist *wlist);
-wordlist * wl_copy(wordlist *wlist);
-wordlist * wl_splice(wordlist *elt, wordlist *list);
-void wl_print(const wordlist *wlist, FILE *fp);
-wordlist * wl_build(char **v);
-char ** wl_mkvec(wordlist *wl);
 wordlist * wl_append(wordlist *wlist, wordlist *nwl);
-wordlist * wl_reverse(wordlist *wl);
-char * wl_flatten(const wordlist *wl);
-wordlist * wl_nthelem(int i, wordlist *wl);
-void wl_sort(wordlist *wl);
-wordlist * wl_range(wordlist *wl, int low, int up);
-
-wordlist *wl_cons(char *word, wordlist *tail);
 void wl_append_word(wordlist **first, wordlist **last, char *word);
-
+wordlist * wl_build(const char * const *v);
 wordlist *wl_chop(wordlist *wlist);
 wordlist *wl_chop_rest(wordlist *wlist);
-
-wordlist *wl_find(const char *string, const wordlist *wlist);
+wordlist *wl_cons(char *word, wordlist *tail);
+wordlist * wl_copy(const wordlist *wlist);
 void wl_delete_slice(wordlist *from, wordlist *to);
+wordlist *wl_find(const char *string, const wordlist *wlist);
+char * wl_flatten(const wordlist *wl);
+void wl_free(wordlist *wlist);
+wordlist *wl_from_string(const char *sz);
+int wl_length(const wordlist *wlist);
+char ** wl_mkvec(const wordlist *wl);
+wordlist * wl_nthelem(int i, wordlist *wl);
+void wl_print(const wordlist *wlist, FILE *fp);
+wordlist * wl_range(wordlist *wl, int low, int up);
+wordlist * wl_reverse(wordlist *wl);
+void wl_sort(wordlist *wl);
+wordlist * wl_splice(wordlist *elt, wordlist *list);
+
+
+
 
 
 #ifdef QUOTE_CHAR
