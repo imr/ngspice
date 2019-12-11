24f26a1dfe19b3149914a83ff5e67a433e72d2ba
Jim Monte <jim.monte01@gmail.com>
2019-12-10 19:47:00 -0500

    Fixed "phantom vector" issue with plots and crash when plots are resized. See bugs #419 and #423 for details. Also several parameters were changed from char * to const char * in the plotting functions.

--- src/frontend/plotting/plotit.h
+++ src/frontend/plotting/plotit.h
@@ -1,6 +1,6 @@
 #ifndef ngspice_PLOTIT_H
 #define ngspice_PLOTIT_H
 
-bool plotit(wordlist *wl, char *hcopy, char *devname);
+bool plotit(wordlist *wl, const char *hcopy, const char *devname);
 
 #endif
