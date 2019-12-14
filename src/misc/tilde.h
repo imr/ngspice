f9e6664929164e11f5fa2fdce778d652e234c6ec
Jim Monte <jim.monte01@gmail.com>
2019-12-14 01:13:13 -0500

    Fixed resolution of ~ to home directory. (Bug #405) Also fixed potential buffer overruns in glob expansion

--- src/misc/tilde.h
+++ src/misc/tilde.h
@@ -6,7 +6,13 @@
 #ifndef ngspice_TILDE_H
 #define ngspice_TILDE_H
 
-char * tildexpand(char *string);
+char * tildexpand(const char *string);
+
+int get_local_home(size_t n_byte_buf, char **p_buf);
 
 
+#ifdef HAVE_PWD_H
+int get_usr_home(const char *usr, size_t n_byte_buf, char **p_buf);
 #endif
+
+#endif /* include guard */
