6ce9003d3d8849cacf938bcdbe0e30a415e6299c
Holger Vogt <holger.vogt@uni-due.de>
2020-02-12 23:13:08 +0100

    update to cmpp by J. Monte

---
+++ src/xspice/icm/dlmain.h
@@ -0,0 +1,10 @@
+#ifndef dlmain_h_included
+#define dlmain_h_included
+
+#include "ngspice/cmproto.h"
+#include "ngspice/mifproto.h"
+#include "ngspice/dllitf.h"
+
+struct coreInfo_t *coreitf;
+
+#endif /* dlmain.h */
