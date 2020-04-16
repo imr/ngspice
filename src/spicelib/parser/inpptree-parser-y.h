499a55e392778d210b1d216cb23e50e12bdbf768
Holger Vogt <holger.vogt@uni-due.de>
2020-04-16 12:02:30 +0200

    define is no longer needed with recent VS 2019

--- src/spicelib/parser/inpptree-parser-y.h
+++ src/spicelib/parser/inpptree-parser-y.h
@@ -8,8 +8,3 @@ extern INPparseNode *PT_mkbnode(const char *opstr, INPparseNode *arg1, INPparseN
 extern INPparseNode *PT_mkfnode(const char *fname, INPparseNode *arg);
 extern INPparseNode *PT_mknnode(double number);
 extern INPparseNode *PT_mksnode(const char *string, void *ckt);
-
-
-#if defined (_MSC_VER)
-# define __func__ __FUNCTION__ /* __func__ is C99, but MSC can't */
-#endif
