a53b190fbfbf2c40e0a3af195589f58defae402c
Florian Ballenegger <florian.ballenegger@anamosic.com>
2020-07-26 00:49:57 +0200

    simd suffix after model version works again

--- src/spicelib/parser/inpdomod.c
+++ src/spicelib/parser/inpdomod.c
@@ -299,7 +299,7 @@ char *INPdomodel(CKTcircuit *ckt, struct card *image, INPtables * tab)
 			      type = INPtypelook("BSIM3v1");
 			    }
 			    if (prefix("3.2", ver)) { /* version string ver has to start with 3.2 */
-			      #ifdef BSIM3v32SIMDxxxxx
+			      #ifdef BSIM3v32SIMD
 			      if((strlen(ver)>5) && (strcmp(&ver[strlen(ver)-4],"simd")==0))
 			        type = INPtypelook("BSIM3v32simd");
 			      else
