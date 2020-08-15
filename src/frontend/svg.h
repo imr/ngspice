bc678baaca90170c45fa52ac6c6cc0601b777d7c
Holger Vogt <holger.vogt@uni-due.de>
2020-08-15 21:59:58 +0200

    modified from Giles Atkinson's patch: use hardcopy entries to set variables maybe overridden by stropts and intopts list variables

---
+++ src/frontend/svg.h
@@ -0,0 +1,17 @@
+/* Header file for SVG.c */
+
+#ifndef ngspice_SVG_H
+#define ngspice_SVG_H
+
+disp_fn_Init_t             SVG_Init;
+disp_fn_NewViewport_t      SVG_NewViewport;
+disp_fn_Close_t            SVG_Close;
+disp_fn_Clear_t            SVG_Clear;
+disp_fn_DrawLine_t         SVG_DrawLine;
+disp_fn_Arc_t              SVG_Arc;
+disp_fn_Text_t             SVG_Text;
+disp_fn_SetLinestyle_t     SVG_SetLinestyle;
+disp_fn_SetColor_t         SVG_SetColor;
+disp_fn_Update_t           SVG_Update;
+
+#endif
