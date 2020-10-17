c29a0085740fa8241d88c0f9d6b74d1632e60b59
Francesco Lannutti <nicolati@users.sf.net>
2020-10-17 21:45:54 +0200

    Add KLU support to VDMOS

--- src/spicelib/devices/vdmos/vdmosdefs.h
+++ src/spicelib/devices/vdmos/vdmosdefs.h
@@ -244,6 +244,54 @@ typedef struct sVDMOSinstance {
     double *VDMOSCktTtpPtr;
     double *VDMOSTpcktTPtr;
 
+#ifdef KLU
+    BindElement *VDMOSTemptempBinding ;
+    BindElement *VDMOSTempdpBinding ;
+    BindElement *VDMOSTempspBinding ;
+    BindElement *VDMOSTempgpBinding ;
+    BindElement *VDMOSGPtempBinding ;
+    BindElement *VDMOSDPtempBinding ;
+    BindElement *VDMOSSPtempBinding ;
+    BindElement *VDIOTempposPrimeBinding ;
+    BindElement *VDIOTempnegBinding ;
+    BindElement *VDIOPosPrimetempBinding ;
+    BindElement *VDIONegtempBinding ;
+    BindElement *VDMOSTcasetcaseBinding ;
+    BindElement *VDMOSTcasetempBinding ;
+    BindElement *VDMOSTemptcaseBinding ;
+    BindElement *VDMOSTptpBinding ;
+    BindElement *VDMOSTptcaseBinding ;
+    BindElement *VDMOSTcasetpBinding ;
+    BindElement *VDMOSCktTcktTBinding ;
+    BindElement *VDMOSCktTtpBinding ;
+    BindElement *VDMOSTpcktTBinding ;
+    BindElement *VDMOSDdBinding ;
+    BindElement *VDMOSGgBinding ;
+    BindElement *VDMOSSsBinding ;
+    BindElement *VDMOSDPdpBinding ;
+    BindElement *VDMOSSPspBinding ;
+    BindElement *VDMOSGPgpBinding ;
+    BindElement *VDMOSDdpBinding ;
+    BindElement *VDMOSGPdpBinding ;
+    BindElement *VDMOSGPspBinding ;
+    BindElement *VDMOSSspBinding ;
+    BindElement *VDMOSDPspBinding ;
+    BindElement *VDMOSDPdBinding ;
+    BindElement *VDMOSDPgpBinding ;
+    BindElement *VDMOSSPgpBinding ;
+    BindElement *VDMOSSPsBinding ;
+    BindElement *VDMOSSPdpBinding ;
+    BindElement *VDMOSGgpBinding ;
+    BindElement *VDMOSGPgBinding ;
+    BindElement *VDMOSDsBinding ;
+    BindElement *VDMOSSdBinding ;
+    BindElement *VDIORPdBinding ;
+    BindElement *VDIODrpBinding ;
+    BindElement *VDIOSrpBinding ;
+    BindElement *VDIORPsBinding ;
+    BindElement *VDIORPrpBinding ;
+#endif
+
 } VDMOSinstance ;
 
 #define VDMOSvgs VDMOSstates+ 0   /* gate-source voltage */
