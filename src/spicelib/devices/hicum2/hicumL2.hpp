af3d3c5230cd769d7d17bbe25cb10a829f6f200c
dwarning <dwarning>
2020-11-10 09:42:37 +0100

    HICUM limitlog exchange

--- src/spicelib/devices/hicum2/hicumL2.hpp
+++ src/spicelib/devices/hicum2/hicumL2.hpp
@@ -12,7 +12,6 @@ extern "C" {
 #endif
     void hicum_diode(double T, double IS, double UM1, double U, double *Iz, double *Gz, double *Tz);
     void hicum_qjmodf(double T, double c_0, double u_d, double z, double a_j, double U_cap, double *C, double *C_dU, double *C_dvt, double *Qz, double *Qz_dU, double *Qz_dvt);
-    static double HICUMlimitlog( double deltemp, double deltemp_old, double LIM_TOL, int *check);
     int hicum_thermal_update(HICUMmodel *, HICUMinstance *, double * Temp, double * Tdev_Vrth);
     int HICUMload(GENmodel *inModel, CKTcircuit *ckt);
     int HICUMtemp(GENmodel *inModel, CKTcircuit *ckt);
