a400b08fa7b375fb9a6985cbfd26e7d2ffada05f
dwarning <dwarning>
2020-11-12 22:27:32 +0100

    HICUM2 rm unused macro M_PI

--- src/spicelib/devices/hicum2/hicumL2.cpp
+++ src/spicelib/devices/hicum2/hicumL2.cpp
@@ -14,9 +14,6 @@ Model Author         : 1990 Michael Schröter TU Dresden
  */
 
 #include <cmath>
-#ifndef M_PI
-#define M_PI 3.1415926535897932384626433832795
-#endif
 #include "duals/dual"
 #include "hicumL2.hpp"
 #include "hicumL2temp.hpp"
--- src/spicelib/devices/hicum2/hicumL2temp.cpp
+++ src/spicelib/devices/hicum2/hicumL2temp.cpp
@@ -5,9 +5,6 @@ Model Author         : 1990 Michael Schröter TU Dresden
 **********/
 
 #include <cmath>
-#ifndef M_PI
-#define M_PI 3.1415926535897932384626433832795
-#endif
 #include "duals/dual"
 #include "hicumL2.hpp"
 #include <functional>
