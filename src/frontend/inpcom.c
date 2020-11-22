7f3f07d282556a80920db1c83bc9d2f071aa4422
Holger Vogt <holger.vogt@uni-due.de>
2020-11-22 17:18:27 +0100

    Partially undo commit [ab778d] : revert to aswitch as vswitch replacement

--- src/frontend/inpcom.c
+++ src/frontend/inpcom.c
@@ -8006,7 +8006,7 @@ static struct card *pspice_compat(struct card *oldcard)
                 tfree(card->line);
                 rep_spar(modpar);
                 card->line = tprintf(
-                        ".model a%s pswitch(%s %s %s %s  log=TRUE)", modname,
+                        ".model a%s aswitch(%s %s %s %s  log=TRUE  limit=TRUE)", modname,
                         modpar[0], modpar[1], modpar[2], modpar[3]);
             }
             for (i = 0; i < 4; i++)
@@ -8187,7 +8187,7 @@ static struct card *pspice_compat(struct card *oldcard)
                 tfree(card->line);
                 rep_spar(modpar);
                 card->line = tprintf(
-                    ".model a%s pswitch(%s %s %s %s  log=TRUE)", modname,
+                    ".model a%s aswitch(%s %s %s %s  log=TRUE  limit=TRUE)", modname,
                     modpar[0], modpar[1], modpar[2], modpar[3]);
             }
             for (i = 0; i < 4; i++)
