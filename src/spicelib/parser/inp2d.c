imr/ngspice

commit 4895a4b5a9a129854888595e9aa46d8d08dc9d91

Diode model with selfheating option

src/spicelib/parser/inp2d.c
@@ -15,78 +15,100 @@ Modified: 2001 Paolo Nenzi (Cider Integration)
 void INP2D(CKTcircuit *ckt, INPtables * tab, struct card *current)
 {
 
-/* Dname <node> <node> <model> [<val>] [OFF] [IC=<val>] */
+/* Dname <node> <node> [<temp>] <model> [<val>] [OFF] [IC=<val>] */
 
-    int mytype;			/* the type we looked up */
-    int type;			/* the type the model says it is */
-    char *line;			/* the part of the current line left to parse */
-    char *name;			/* the resistor's name */
-    char *nname1;		/* the first node's name */
-    char *nname2;		/* the second node's name */
-    CKTnode *node1;		/* the first node's node pointer */
-    CKTnode *node2;		/* the second node's node pointer */
-    int error;			/* error code temporary */
-    GENinstance *fast;		/* pointer to the actual instance */
-    IFvalue ptemp;		/* a value structure to package resistance into */
-    int waslead;		/* flag to indicate that funny unlabeled number was found */
-    double leadval;		/* actual value of unlabeled number */
-    char *model;		/* the name of the model */
-    INPmodel *thismodel;	/* pointer to model description for user's model */
-    GENmodel *mdfast;		/* pointer to the actual model */
-    IFuid uid;			/* uid of default model */
+    int mytype;         /* the type we looked up */
+    int type;           /* the type the model says it is */
+    char *line;         /* the part of the current line left to parse */
+    char *name;         /* the resistor's name */
+    const int max_i = 3;/* the maximum node count */
+    CKTnode *node[3];   
+    int error;          /* error code temporary */
+    int numnodes;       /* flag indicating 4 or 5 nodes */
+    GENinstance *fast;  /* pointer to the actual instance */
+    IFvalue ptemp;      /* a value structure to package resistance into */
+    int waslead;        /* flag to indicate that funny unlabeled number was found */
+    double leadval;     /* actual value of unlabeled number */
+    INPmodel *thismodel;/* pointer to model description for user's model */
+    GENmodel *mdfast;   /* pointer to the actual model */
+    IFuid uid;          /* uid of default model */
+    int i;
 
     mytype = INPtypelook("Diode");
     if (mytype < 0) {
-	LITERR("Device type Diode not supported by this binary\n");
-	return;
+    LITERR("Device type Diode not supported by this binary\n");
+    return;
     }
     line = current->line;
     INPgetNetTok(&line, &name, 1);
     INPinsert(&name, tab);
-    INPgetNetTok(&line, &nname1, 1);
-    INPtermInsert(ckt, &nname1, tab, &node1);
-    INPgetNetTok(&line, &nname2, 1);
-    INPtermInsert(ckt, &nname2, tab, &node2);
-    INPgetNetTok(&line, &model, 1);
-    INPinsert(&model, tab);
-    current->error = INPgetMod(ckt, model, &thismodel, tab);
-    if (thismodel != NULL) {
-	if ((mytype != thismodel->INPmodType)
 
-#ifdef CIDER
-                      && (thismodel->INPmodType != INPtypelook("NUMD"))
-                      && (thismodel->INPmodType != INPtypelook("NUMD2"))
-#endif
-       ){
-	    LITERR("incorrect model type");
-	    return;
-	}
-	type = thismodel->INPmodType; /*HT 050903*/
-	mdfast = (thismodel->INPmodfast);
-    } else {
-	type = mytype;
-	if (!tab->defDmod) {
-	    /* create default D model */
-	    IFnewUid(ckt, &uid, NULL, "D", UID_MODEL, NULL);
-	    IFC(newModel, (ckt, type, &(tab->defDmod), uid));
-	}
-	mdfast = tab->defDmod;
+    for (i = 0; ; i++) {
+        char *token;
+        INPgetNetTok(&line, &token, 1);
+        if (i >= 2 && INPlookMod(token)) {
+            INPinsert(&token, tab);
+            txfree(INPgetMod(ckt, token, &thismodel, tab));
+            if (!thismodel) {
+                LITERR ("Unable to find definition of given model");
+                return;
+            }
+            break;
+        }
+        if (i >= max_i) {
+            LITERR ("could not find a valid modelname");
+            return;
+        }
+        INPtermInsert(ckt, &token, tab, &node[i]);
     }
+
+    if (i > max_i) {
+        LITERR("Too many nodes for this model type");
+        return;
+    }
+
+    numnodes = i;
+
+    if (thismodel != NULL) {
+        if ((mytype != thismodel->INPmodType)
+#ifdef CIDER
+            && (thismodel->INPmodType != INPtypelook("NUMD"))
+            && (thismodel->INPmodType != INPtypelook("NUMD2"))
+#endif
+        ) {
+            LITERR("incorrect model type");
+            return;
+        }
+        type = thismodel->INPmodType; /*HT 050903*/
+        mdfast = (thismodel->INPmodfast);
+    } else {
+        type = mytype;
+        if (!tab->defDmod) {
+            /* create default D model */
+            IFnewUid(ckt, &uid, NULL, "D", UID_MODEL, NULL);
+            IFC(newModel, (ckt, type, &(tab->defDmod), uid));
+        }
+        mdfast = tab->defDmod;
+    }
+
     IFC(newInstance, (ckt, mdfast, &fast, name));
-    IFC(bindNode, (ckt, fast, 1, node1));
-    IFC(bindNode, (ckt, fast, 2, node2));
+    for (i = 0; i < max_i; i++)
+        if (i < numnodes)
+            IFC (bindNode, (ckt, fast, i + 1, node[i]));
+        else
+            GENnode(fast)[i] = -1;
+
     PARSECALL((&line, ckt, type, fast, &leadval, &waslead, tab));
     if (waslead) {
-
 #ifdef CIDER
-    	if( type == INPtypelook("NUMD2") ) {
+        if( type == INPtypelook("NUMD2") ) {
             LITERR(" error:  no unlabelled parameter permitted on NUMD2\n");
-	} else {
+    } else {
 #endif
-	ptemp.rValue = leadval;
-	GCA(INPpName, ("area", &ptemp, ckt, type, fast));
+        ptemp.rValue = leadval;
+        GCA(INPpName, ("area", &ptemp, ckt, type, fast));
     }
 #ifdef CIDER    
-  }
+    }
 #endif
 }

src/frontend/inpcom.c
@@ -4277,7 +4277,17 @@ int get_number_terminals(char *c)
         case 'v':
         case 'i':
         case 'd':
-            return 2;
+            i = 0;
+            /* find the first token with "off" or "=" in the line*/
+            while ((i < 10) && (*c != '\0')) {
+                char *inst = gettok_instance(&c);
+                strncpy(nam_buf, inst, sizeof(nam_buf) - 1);
+                txfree(inst);
+                if (strstr(nam_buf, "off") || strstr(nam_buf, "thermal") || strchr(nam_buf, '='))
+                    break;
+                i++;
+            }
+            return i - 2;
             break;
         case 'u':
         case 'j':

src/frontend/subckt.c
@@ -1648,7 +1648,7 @@ numnodes(const char *line, struct subs *subs, wordlist const *modnames)
 
     /* If model names equal node names, this code will fail! */
 
-    if ((c == 'm') || (c == 'p') || (c == 'q')) { /* IF this is a mos, cpl or bjt*/
+    if ((c == 'm') || (c == 'p') || (c == 'q') || (c == 'd')) { /* IF this is a mos, cpl, bjt or diode */
         char *s = nexttok(line);       /* Skip the instance name */
         int gotit = 0;
         int i = 0;
@@ -1928,7 +1928,27 @@ devmodtranslate(struct card *s, char *subname, wordlist * const orig_modnames)
             name = gettok_node(&t);  /* get second attached netname */
             bxx_printf(&buffer, "%s ", name);
             tfree(name);
-            name = gettok(&t);
+            name = gettok_node(&t);  /* this can be either a model name or a node name. */
+            if (name == NULL) {
+                name = copy(""); /* allow 'tfree' */
+            } else {
+                found = 0;
+                while (!found) {
+                    wlsub = wl_find(name, orig_modnames);
+                    if (wlsub) {
+                        found = 1;
+                        break;
+                    } else {
+                        bxx_printf(&buffer, "%s ", name);
+                        tfree(name);
+                        name = gettok(&t);
+                        if (name == NULL) {  /* No token anymore - leave */
+                            name = copy(""); /* allow 'tfree' */
+                            break;
+                        }
+                    }
+                }  /* while  */
+            }
 
             translate_mod_name(&buffer, name, subname, orig_modnames);
 
@@ -2162,7 +2182,7 @@ inp_numnodes(char c)
     case 'c':
         return (2);
     case 'd':
-        return (2);
+        return (3);
     case 'e':
         return (2); /* changed from 4 to 2 by SDB on 4.22.2003 to enable POLY */
     case 'f':

src/spicelib/devices/dio/dio.c
@@ -23,6 +23,7 @@ IFparm DIOpTable[] = { /* parameters */
  IOPU("lp",    DIO_LP,    IF_REAL, "Length of polysilicon capacitor (level=3)"),
  IOPU("wm",    DIO_WM,    IF_REAL, "Width of metal capacitor (level=3)"),
  IOPU("wp",    DIO_WP,    IF_REAL, "Width of polysilicon capacitor (level=3)"),
+ IOP("thermal",DIO_THERMAL, IF_FLAG, "Self heating mode selector"),
 
  IP("sens_area",DIO_AREA_SENS,IF_FLAG,"flag to request sensitivity WRT area"),
  OP("vd",      DIO_VOLTAGE,IF_REAL, "Diode voltage"),
@@ -112,7 +113,10 @@ IFparm DIOmPTable[] = { /* model parameters */
  IOP( "bv_max",   DIO_MOD_BV_MAX,  IF_REAL, "maximum voltage in reverse direction"),
  IOP( "isr",  DIO_MOD_ISR,  IF_REAL, "Recombination saturation current"),
  IOP( "nr",   DIO_MOD_NR,   IF_REAL, "Recombination current emission coefficient"),
-
+/* self heating */
+ IOP("rth0",  DIO_MOD_RTH0,  IF_REAL, "Self-heating thermal resistance"),
+ IOP("cth0",  DIO_MOD_CTH0,  IF_REAL, "Self-heating thermal capacitance"),
+/* scaled parasitic capacitances level 3 model */
  IOP( "lm",  DIO_MOD_LM,  IF_REAL, "Length of metal capacitor (level=3)"),
  IOP( "lp",  DIO_MOD_LP,  IF_REAL, "Length of polysilicon capacitor (level=3)"),
  IOP( "wm",  DIO_MOD_WM,  IF_REAL, "Width of metal capacitor (level=3)"),
@@ -127,7 +131,8 @@ IFparm DIOmPTable[] = { /* model parameters */
 
 char *DIOnames[] = {
     "D+",
-    "D-"
+    "D-",
+    "Tj"
 };
 
 int DIOnSize = NUMELEMS(DIOnames);

src/spicelib/devices/dio/dioacld.c
@@ -28,7 +28,7 @@ DIOacLoad(GENmodel *inModel, CKTcircuit *ckt)
         /* loop through all the instances of the model */
         for (here = DIOinstances(model); here != NULL ;
                 here=DIOnextInstance(here)) {
-            gspr=here->DIOtConductance*here->DIOarea;
+            gspr=here->DIOtConductance;
             geq= *(ckt->CKTstate0 + here->DIOconduct);
             xceq= *(ckt->CKTstate0 + here->DIOcapCurrent) * ckt->CKTomega;
             *(here->DIOposPosPtr ) += gspr;
@@ -42,6 +42,25 @@ DIOacLoad(GENmodel *inModel, CKTcircuit *ckt)
             *(here->DIOposPrimePosPtr ) -= gspr;
             *(here->DIOposPrimeNegPtr ) -= geq;
             *(here->DIOposPrimeNegPtr +1 ) -= xceq;
+            int selfheat = ((here->DIOtempNode > 0) && (here->DIOthermal) && (model->DIOrth0Given));
+            if (selfheat) {
+                double dIth_dVrs = here->DIOdIth_dVrs;
+                double dIth_dVdio = here->DIOdIth_dVdio;
+                double dIth_dT = here->DIOdIth_dT;
+                double gcTt = here->DIOgcTt;
+                double dIrs_dT = here->DIOdIrs_dT;
+                double dIdio_dT = *(ckt->CKTstate0 + here->DIOdIdio_dT);
+                (*(here->DIOtempPosPtr)      += -dIth_dVrs);
+                (*(here->DIOtempPosPrimePtr) += -dIth_dVdio + dIth_dVrs);
+                (*(here->DIOtempNegPtr)      +=  dIth_dVdio);
+                (*(here->DIOtempTempPtr)     += -dIth_dT + 1/model->DIOrth0 + gcTt);
+                (*(here->DIOposTempPtr)      +=  dIrs_dT);
+                (*(here->DIOposPrimeTempPtr) +=  dIdio_dT - dIrs_dT);
+                (*(here->DIOnegTempPtr)      += -dIdio_dT);
+
+                double xgcTt= *(ckt->CKTstate0 + here->DIOcqth) * ckt->CKTomega;
+                (*(here->DIOtempTempPtr + 1) +=  xgcTt);
+            }
         }
     }
     return(OK);

src/spicelib/devices/dio/dioask.c
@@ -48,6 +48,9 @@ DIOask (CKTcircuit *ckt, GENinstance *inst, int which, IFvalue *value,
         case DIO_M:
             value->rValue = here->DIOm;
             return(OK);
+        case DIO_THERMAL:
+            value->iValue = here->DIOthermal;
+            return(OK);
 
         case DIO_TEMP:
             value->rValue = here->DIOtemp-CONSTCtoK;
@@ -81,7 +84,9 @@ DIOask (CKTcircuit *ckt, GENinstance *inst, int which, IFvalue *value,
                 return(E_ASKPOWER);
             } else {
                 value->rValue = *(ckt->CKTstate0 + here->DIOcurrent) *
-                        *(ckt->CKTstate0 + here->DIOvoltage);
+                        *(ckt->CKTstate0 + here->DIOvoltage) +
+                        *(ckt->CKTstate0 + here->DIOcurrent) *
+                        *(ckt->CKTstate0 + here->DIOcurrent) / here->DIOtConductance;
             }
             return(OK);
         case DIO_QUEST_SENS_DC:

src/spicelib/devices/dio/dioconv.c
@@ -21,6 +21,7 @@ DIOconvTest(GENmodel *inModel, CKTcircuit *ckt)
     DIOinstance *here;
     double delvd,vd,cdhat,cd;
     double tol;
+    double delTemp, deldelTemp;
     /*  loop through all the diode models */
     for( ; model != NULL; model = DIOnextModel(model)) {
 
@@ -36,8 +37,17 @@ DIOconvTest(GENmodel *inModel, CKTcircuit *ckt)
                     *(ckt->CKTrhsOld + here->DIOnegNode);
 
             delvd=vd- *(ckt->CKTstate0 + here->DIOvoltage);
+
+            int selfheat = ((here->DIOtempNode > 0) && (here->DIOthermal) && (model->DIOrth0Given));
+            if (selfheat)
+                delTemp = *(ckt->CKTrhsOld + here->DIOtempNode);
+            else
+                delTemp = 0.0;
+            deldelTemp = delTemp - *(ckt->CKTstate0 + here->DIOdeltemp);
+
             cdhat= *(ckt->CKTstate0 + here->DIOcurrent) + 
-                    *(ckt->CKTstate0 + here->DIOconduct) * delvd;
+                    *(ckt->CKTstate0 + here->DIOconduct) * delvd +
+                    *(ckt->CKTstate0 + here->DIOdIdio_dT) * deldelTemp;
 
             cd= *(ckt->CKTstate0 + here->DIOcurrent);
 
@@ -48,7 +58,7 @@ DIOconvTest(GENmodel *inModel, CKTcircuit *ckt)
                     MAX(fabs(cdhat),fabs(cd))+ckt->CKTabstol;
             if (fabs(cdhat-cd) > tol) {
                 ckt->CKTnoncon++;
-		ckt->CKTtroubleElt = (GENinstance *) here;
+                ckt->CKTtroubleElt = (GENinstance *) here;
                 return(OK); /* don't need to check any more device */
             }
         }

src/spicelib/devices/dio/diodefs.h
@@ -38,7 +38,8 @@ typedef struct sDIOinstance {
 
     const int DIOposNode;     /* number of positive node of diode */
     const int DIOnegNode;     /* number of negative node of diode */
-    int DIOposPrimeNode;    /* number of positive prime node of diode */
+    const int DIOtempNode;    /* number of the temperature node of the diode */
+    int DIOposPrimeNode;      /* number of positive prime node of diode */
 
     double *DIOposPosPrimePtr;      /* pointer to sparse matrix at 
                                      * (positive,positive prime) */
@@ -54,6 +55,15 @@ typedef struct sDIOinstance {
                              * (negative,negative) */
     double *DIOposPrimePosPrimePtr; /* pointer to sparse matrix at 
                                      * (positive prime,positive prime) */
+
+    /* self heating */
+    double *DIOtempPosPtr;
+    double *DIOtempPosPrimePtr;
+    double *DIOtempNegPtr;
+    double *DIOtempTempPtr;
+    double *DIOposTempPtr;
+    double *DIOposPrimeTempPtr;
+    double *DIOnegTempPtr;
 
     double DIOcap;   /* stores the diode capacitance */
 
@@ -86,6 +96,7 @@ typedef struct sDIOinstance {
     double DIOw;        /* width for the diode */
     double DIOl;        /* length for the diode */
     double DIOm;        /* multiplier for the diode */
+    int    DIOthermal;  /* flag indicate self heating on */
 
     double DIOlengthMetal;   /* Length of metal capacitor (level=3) */
     double DIOlengthPoly;    /* Length of polysilicon capacitor (level=3) */
@@ -101,16 +112,21 @@ typedef struct sDIOinstance {
     double DIOtJctSWCap;     /* temperature adjusted sidewall junction capacitance */
     double DIOtTransitTime;  /* temperature adjusted transit time */
     double DIOtGradingCoeff; /* temperature adjusted grading coefficient (MJ) */
-    double DIOtConductance;  /* temperature adjusted series conductance */
+    double DIOtConductance;    /* temperature adjusted series conductance */
+    double DIOtConductance_dT; /* temperature adjusted series conductance temperature derivative */
 
-    double DIOtDepCap;  /* temperature adjusted transition point in */
-                        /* the curve matching (Fc * Vj ) */
-    double DIOtDepSWCap;  /* temperature adjusted transition point in */
-                          /* the curve matching (Fcs * Vjs ) */
-    double DIOtSatCur;  /* temperature adjusted saturation current */
-    double DIOtSatSWCur;  /* temperature adjusted side wall saturation current */
-    double DIOtTunSatCur;        /* tunneling saturation current */
-    double DIOtTunSatSWCur;      /* sidewall tunneling saturation current */
+    double DIOtDepCap;       /* temperature adjusted transition point in */
+                             /* the curve matching (Fc * Vj ) */
+    double DIOtDepSWCap;     /* temperature adjusted transition point in */
+                             /* the curve matching (Fcs * Vjs ) */
+    double DIOtSatCur;         /* temperature adjusted saturation current */
+    double DIOtSatCur_dT;      /* temperature adjusted saturation current temperature derivative */
+    double DIOtSatSWCur;       /* temperature adjusted side wall saturation current */
+    double DIOtSatSWCur_dT;    /* temperature adjusted side wall saturation current temperature derivative */
+    double DIOtTunSatCur;      /* tunneling saturation current */
+    double DIOtTunSatCur_dT;   /* tunneling saturation current temperature derivative */
+    double DIOtTunSatSWCur;    /* sidewall tunneling saturation current */
+    double DIOtTunSatSWCur_dT; /* sidewall tunneling saturation current temperature derivative */
 
     double DIOtVcrit;   /* temperature adjusted V crit */
     double DIOtF1;      /* temperature adjusted f1 */
@@ -123,9 +139,17 @@ typedef struct sDIOinstance {
 
     double DIOforwardKneeCurrent; /* Forward Knee current */
     double DIOreverseKneeCurrent; /* Reverse Knee current */
-    double DIOjunctionCap;     /* geometry adjusted junction capacitance */
-    double DIOjunctionSWCap;     /* geometry adjusted junction sidewall capacitance */
-    double DIOtRecSatCur; /* temperature adjusted recombination saturation current */
+    double DIOjunctionCap;        /* geometry adjusted junction capacitance */
+    double DIOjunctionSWCap;      /* geometry adjusted junction sidewall capacitance */
+    double DIOtRecSatCur;         /* temperature adjusted recombination saturation current */
+    double DIOtRecSatCur_dT;      /* temperature adjusted recombination saturation current */
+
+    double DIOdIth_dVrs;
+    double DIOdIth_dVdio;
+    double DIOdIth_dT;
+    double DIOgcTt;
+    double DIOdIrs_dT;
+    double DIOdIdio_dT;
 
     double DIOcmetal; /* parasitic metal overlap capacitance */
     double DIOcpoly;  /* parasitic polysilicon overlap capacitance */
@@ -175,10 +199,16 @@ typedef struct sDIOinstance {
 #define DIOcapCharge DIOstate+3
 #define DIOcapCurrent DIOstate+4
 
-#define DIOnumStates 5
+#define DIOqth DIOstate+5     /* thermal capacitor charge */
+#define DIOcqth DIOstate+6    /* thermal capacitor current */
 
-#define DIOsensxp DIOstate+5    /* charge sensitivities and their derivatives.
-                                 * +6 for the derivatives - pointer to the
+#define DIOdeltemp DIOstate+7 /* thermal voltage over rth0 */
+#define DIOdIdio_dT DIOstate+8
+
+#define DIOnumStates 9
+
+#define DIOsensxp DIOstate+9    /* charge sensitivities and their derivatives.
+                                 * +10 for the derivatives - pointer to the
                                  * beginning of the array */
 
 #define DIOnumSenStates 2
@@ -248,6 +278,9 @@ typedef struct sDIOmodel {       /* model structure for a diode */
     unsigned DIOrecSatCurGiven : 1;
     unsigned DIOrecEmissionCoeffGiven : 1;
 
+    unsigned DIOrth0Given :1;
+    unsigned DIOcth0Given :1;
+
     unsigned DIOlengthMetalGiven : 1;     /* Length of metal capacitor (level=3) */
     unsigned DIOlengthPolyGiven : 1;      /* Length of polysilicon capacitor (level=3) */
     unsigned DIOwidthMetalGiven : 1;      /* Width of metal capacitor (level=3) */
@@ -312,6 +345,9 @@ typedef struct sDIOmodel {       /* model structure for a diode */
     double DIOrecSatCur; /* Recombination saturation current */
     double DIOrecEmissionCoeff; /* Recombination emission coefficient */
 
+    double DIOrth0;
+    double DIOcth0;
+
     double DIOlengthMetal;     /* Length of metal capacitor (level=3) */
     double DIOlengthPoly;      /* Length of polysilicon capacitor (level=3) */
     double DIOwidthMetal;      /* Width of metal capacitor (level=3) */
@@ -348,6 +384,7 @@ enum {
     DIO_L,
     DIO_M,
     DIO_DTEMP,
+    DIO_THERMAL,
     DIO_LM,
     DIO_LP,
     DIO_WM,
@@ -407,6 +444,8 @@ enum {
     DIO_MOD_BV_MAX,
     DIO_MOD_ISR,
     DIO_MOD_NR,
+    DIO_MOD_RTH0,
+    DIO_MOD_CTH0,
 
     DIO_MOD_LM,
     DIO_MOD_LP,
@@ -417,6 +456,8 @@ enum {
     DIO_MOD_XM,
     DIO_MOD_XP,
 };
+
+void DIOtempUpdate(DIOmodel *inModel, DIOinstance *here, double Temp, CKTcircuit *ckt);
 
 #include "dioext.h"
 #endif /*DIO*/

commit f6cdf4b95b0b2eda27609077ae15544b86f8f28b

Prevent crash when CIDER diode models are used. There is no third node in NUMD or NUMD2, so don't set GENnode(fast)[i]. This patch fixes a bug introduced in 4895a4b5a ("Diode model with selfheating option", 2021-04-11)

src/spicelib/parser/inp2d.c
@@ -95,7 +95,8 @@ void INP2D(CKTcircuit *ckt, INPtables * tab, struct card *current)
     for (i = 0; i < max_i; i++)
         if (i < numnodes)
             IFC (bindNode, (ckt, fast, i + 1, node[i]));
-        else
+        else if (thismodel->INPmodType != INPtypelook("NUMD")
+            && (thismodel->INPmodType != INPtypelook("NUMD2")))
             GENnode(fast)[i] = -1;
 
     PARSECALL((&line, ckt, type, fast, &leadval, &waslead, tab));