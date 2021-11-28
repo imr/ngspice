7f75b2a7b8a48256f644430aaf5f805ced43d607
Holger Vogt <holger.vogt@uni-due.de>
2021-11-28 14:41:04 +0100

    Update to previous commit: safeguard against no circuit loaded

--- src/frontend/typesdef.c
+++ src/frontend/typesdef.c
@@ -359,7 +359,7 @@ com_stype(wordlist *wl)
 
     for (wl = wl->wl_next; wl; wl = wl->wl_next) {
         const char* vecname = wl->wl_word;
-        if (*vecname == '@'&& !ft_curckt->ci_runonce) {
+        if (*vecname == '@' && ft_curckt && !ft_curckt->ci_runonce) {
             fprintf(cp_err, "Warning: Vector %s is available only after the simulation has been run!\n", vecname);
             fprintf(cp_err, "    Command 'settype %s %s' is ignored\n\n", type, vecname);
             continue;
