74787f0c33bfc05625f1c510bc23ee5525caef05
roman <roman@lemonsolutions.info>
2022-01-16 19:20:38 -0800

    Fix EXTERNAL type in isrc

--- src/spicelib/devices/isrc/isrc.c
+++ src/spicelib/devices/isrc/isrc.c
@@ -26,7 +26,7 @@ IFparm ISRCpTable[] = { /* parameters */
  IOP ("trnoise", ISRC_TRNOISE,   IF_REALVEC,"Transient noise description"),
  IOP ("trrandom", ISRC_TRRANDOM, IF_REALVEC,"random source description"),
 #ifdef SHARED_MODULE
- IOP ("external", ISRC_EXTERNAL, IF_REALVEC,"external source description"),
+ IOP ("external", ISRC_EXTERNAL, IF_STRING,"external source description"),
 #endif
  OPU ("pos_node",ISRC_POS_NODE,  IF_INTEGER,"Positive node of source"),
  OPU ("neg_node",ISRC_NEG_NODE,  IF_INTEGER,"Negative node of source"),
