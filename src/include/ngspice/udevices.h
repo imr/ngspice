1d40ffb801656ea6305a03db0f9c332bb9033003
Brian Taylor <lbwnet@comcast.net>
2022-04-27 19:59:40 -0700

    Initial files for translating Pspice u.. instances and timing models into Xspice equivalents. Standard gates, tristate gates, dff, jkff, dlatch are supported. Compound gates and pullup/down are not done. Makefile and frontend/inpcom.c changes have not been finished. These modifications will be needed to complete integration.

---
+++ src/include/ngspice/udevices.h
@@ -0,0 +1,10 @@
+#ifndef INCLUDED_UDEVICES
+#define INCLUDED_UDEVICES
+
+BOOL u_process_instance(char *line);
+BOOL u_process_model_line(char *line);
+BOOL u_check_instaance(char *line);
+void create_model_xlator(void);
+void cleanup_model_xlator(void);
+
+#endif
