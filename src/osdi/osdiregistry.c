17c1a2475cff750a0056ee0ec9cfcca5a9f8491e
DSPOM <dspom@protonmail.com>
2022-07-08 15:39:59 +0200

    fix: free of undefined local variable

--- src/osdi/osdiregistry.c
+++ src/osdi/osdiregistry.c
@@ -230,7 +230,6 @@ extern OsdiObjectFile load_object_file(const char *input) {
   const char *path = resolve_input_path(input);
   if (!path) {
     printf("Error opening osdi lib \"%s\": No such file or directory!\n", input);
-    FREE_DLERR_MSG(error);
     return INVALID_OBJECT;
   }
 
