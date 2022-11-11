5adef0cf36e8507470a3fc25a8cd78e0c0a2dfd5
Holger Vogt <holger.vogt@uni-due.de>
2022-11-11 11:50:53 +0100

    AlmostEqualUlps setting has been too strict: it failed in MINGW gcc. 3 --> 10

--- src/spicelib/parser/ptfuncs.c
+++ src/spicelib/parser/ptfuncs.c
@@ -77,8 +77,8 @@ PTpower(double arg1, double arg2)
         else {
             /* If arg2 is quasi an integer, round it to have pow not fail
                when arg1 is negative. Takes into account the double 
-               representation which sometimes differs in the last digit. */
-            if (AlmostEqualUlps(trunc(arg2), arg2, 3))
+               representation which sometimes differs in the last digit(s). */
+            if (AlmostEqualUlps(trunc(arg2), arg2, 10))
                 res = pow(arg1, round(arg2));
             else
                 /* As per LTSPICE specification for ** */
