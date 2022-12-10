a7fced9327d47362d5a1a8c1a92e87bbc7788908
Pascal Kuthe <pascal.kuthe@semimod.de>
2022-12-10 01:55:43 +0100

    ensure compilation succedes without OSDI

--- src/spicelib/parser/inpxx.h
+++ src/spicelib/parser/inpxx.h
@@ -20,7 +20,9 @@ void INP2J(CKTcircuit *ckt, INPtables *tab, struct card *current);
 void INP2K(CKTcircuit *ckt, INPtables *tab, struct card *current);
 void INP2L(CKTcircuit *ckt, INPtables *tab, struct card *current);
 void INP2M(CKTcircuit *ckt, INPtables *tab, struct card *current);
+#ifdef OSDI
 void INP2N(CKTcircuit *ckt, INPtables *tab, struct card *current);
+#endif
 void INP2O(CKTcircuit *ckt, INPtables *tab, struct card *current);
 void INP2P(CKTcircuit *ckt, INPtables *tab, struct card *current);
 void INP2Q(CKTcircuit *ckt, INPtables *tab, struct card *current, CKTnode *gnode);
