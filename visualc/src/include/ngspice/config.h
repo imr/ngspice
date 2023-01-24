71002aa42fbe3fd7af694cad399ed8f3eb519a1e
Holger Vogt <holger.vogt@uni-due.de>
2023-01-24 15:49:42 +0100

    Prepare for ngspice-39

--- visualc/src/include/ngspice/config.h
+++ visualc/src/include/ngspice/config.h
@@ -15,7 +15,7 @@
 #define PACKAGE "ngspice"
 
 /* Version number of package */
-#define VERSION "38+"
+#define VERSION "39"
 
 /* Define the directory for executables */
 #define NGSPICEBINDIR "../bin"
