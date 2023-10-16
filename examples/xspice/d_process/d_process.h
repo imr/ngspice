fb3866d2bffd81acb647d485fc0dcd29146a784b
Brian Taylor <lbwnet@comcast.net>
2023-10-16 13:19:43 -0700

    Error handling improvements in cfunc.mod. Ensure that d_process.h wiil always respond to version and interface checks sent from sendheader. This is needed so that the pipe reads in sendheader do not cause Windows to hang when the interface version and in/out counts do not match. This hang was the cause of errors not being reported and the Windows gui hanging. Startup and header checks are now detected in cm_d_process, and the simulator will run but with runtime errors since a d_process model cannot be completely instantiated after initial errors. It would be good to find a means of gracefully halting the simulation run.

--- examples/xspice/d_process/d_process.h
+++ examples/xspice/d_process/d_process.h
@@ -4,6 +4,15 @@
  * \author Uros Platise <uros@isotel.eu>
  */
 
+/*
+MODIFICATIONS
+
+  16 October 2023 Brian Taylor
+    Always send an acknowledgement to cm_d_process to
+    avoid the pipe read hanging waiting for a response.
+
+*/
+
 #ifndef __NGSPICE_D_PROCESS__
 #define __NGSPICE_D_PROCESS__
 
@@ -28,6 +37,7 @@ static inline int d_process_init(int pipein, int pipeout, uint8_t N_din, uint8_t
         uint8_t version, N_din, N_dout;
     } __attribute__((packed))header;
 #endif
+    int retval = 1;
 
 #if defined(_MSC_VER) || defined(__MINGW64__)
     if (_read(pipein, &header, sizeof(header)) != sizeof(header)) {
@@ -35,19 +45,29 @@ static inline int d_process_init(int pipein, int pipeout, uint8_t N_din, uint8_t
     if (read(pipein, &header, sizeof(header)) != sizeof(header)) {
 #endif
         fprintf(stderr, "Error: Incompatible ngspice d_process header size, requiring version %d\n", header.version);
-        return 0;
+        header.version = 0;
+        header.N_din = 0;
+        header.N_dout = 0;
+        retval = 0;
+    }
+    if (retval == 0) {
+        goto respond;
     }
     if (header.version != D_PROCESS_FORMAT_VERSION || header.N_din != N_din || header.N_dout != N_dout) {
         fprintf(stderr, "Error: Incompatible ngspice d_process requiring version %d, number of inputs %d expected %d, and outputs %d expected %d.\n",
             header.version, header.N_din, N_din, header.N_dout, N_dout);
-        return 0;
+        header.version = D_PROCESS_FORMAT_VERSION;
+        header.N_din = N_din;
+        header.N_dout = N_dout;
+        retval = 0;
     }
+respond:
 #if defined(_MSC_VER) || defined(__MINGW64__)
-    _write(pipeout, &header, sizeof(header));    // acknowledge connection by returning back the same header
+    _write(pipeout, &header, sizeof(header));    // acknowledge connection by returning back the header
 #else
-    write(pipeout, &header, sizeof(header));    // acknowledge connection by returning back the same header
+    write(pipeout, &header, sizeof(header));    // acknowledge connection by returning back the header
 #endif
-    return 1;
+    return retval;
 }
 
 #endif
