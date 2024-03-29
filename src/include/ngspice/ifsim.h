ce8bce79477198e9df96fbbfd01aaf5700544da2
Holger Vogt <holger.vogt@uni-due.de>
2024-03-29 17:23:59 +0100

    Define the members of the struct in the correct sequence.

--- src/include/ngspice/ifsim.h
+++ src/include/ngspice/ifsim.h
@@ -282,6 +282,8 @@ struct IFdevice {
     int *numModelParms;         /* number of model parameter descriptors */
     IFparm *modelParms;         /* array  of model parameter descriptors */
 
+    int flags;          /* DEV_ */
+
 /* gtri - modify - wbk - 10/11/90 - add entries to hold data required */
 /*                                  by new parser                     */
 #ifdef XSPICE
@@ -297,8 +299,6 @@ struct IFdevice {
     Mif_Inst_Var_Info_t *inst_var; /* array of instance var info for mif parser */
 /* gtri - end - wbk - 10/11/90 */
 #endif
-
-    int flags;          /* DEV_ */
 
 #ifdef OSDI
     const void *registry_entry;
