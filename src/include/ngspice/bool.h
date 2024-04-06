4ed1ebf874a73cc8cf1397cc88ad93b27b802bab
Holger Vogt <holger.vogt@uni-due.de>
2024-04-06 19:12:35 +0200

    avoid crash when compiled with lto

--- src/include/ngspice/bool.h
+++ src/include/ngspice/bool.h
@@ -1,20 +1,20 @@
 #ifndef ngspice_BOOL_H
 #define ngspice_BOOL_H
 
-//typedef unsigned char bool;
+#if defined (__MINGW32__) || defined (_MSC_VER)
 #ifndef __cplusplus
 typedef int bool;
 #endif
+#else
+#include <stdbool.h>
+#endif
 
 typedef int BOOL;
-
 
 #define BOOLEAN int
 #define TRUE  1
 #define FALSE 0
 #define NO    0
 #define YES   1
-
-
 
 #endif
