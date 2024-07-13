2af390f0b12ec460f29464d7325cf3ab5b02d98b
Holger Vogt <holger.vogt@uni-due.de>
2024-07-13 10:14:02 +0200

    Update copyright info

--- src/frontend/misccoms.c
+++ src/frontend/misccoms.c
@@ -196,7 +196,7 @@ com_version(wordlist *wl)
 
                 "** The U. C. Berkeley CAD Group\n"
                 "** Copyright 1985-1994, Regents of the University of California.\n"
-                "** Copyright 2001-2023, The ngspice team.\n"
+                "** Copyright 2001-2024, The ngspice team.\n"
                 "** %s\n",
                 ft_sim->simulator, ft_sim->version, ft_sim->description, Spice_Manual);
         if (*Spice_Notice != '\0')
@@ -240,7 +240,7 @@ com_version(wordlist *wl)
 
                     "** The U. C. Berkeley CAD Group\n"
                     "** Copyright 1985-1994, Regents of the University of California.\n"
-                    "** Copyright 2001-2023, The ngspice team.\n"
+                    "** Copyright 2001-2024, The ngspice team.\n"
                     "** %s\n",
                     ft_sim->simulator, ft_sim->version, ft_sim->description, Spice_Manual);
             if (*Spice_Notice != '\0')
