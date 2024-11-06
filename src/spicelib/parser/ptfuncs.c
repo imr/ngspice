8c806912d0d9ccaa210a0b184262027fed357a6f
Holger Vogt <holger.vogt@uni-due.de>
2024-11-06 15:35:38 +0100

    Improve convergence in Infineon Power MOS. 0**something is 0

--- src/spicelib/parser/ptfuncs.c
+++ src/spicelib/parser/ptfuncs.c
@@ -70,7 +70,9 @@ PTpower(double arg1, double arg2)
 {
     double res;
     if (newcompat.lt) {
-        if(arg1 >= 0)
+        if (arg1 == 0)
+            res = 0;
+        else if(arg1 > 0)
             res = pow(arg1, arg2);
         else {
             /* If arg2 is quasi an integer, round it to have pow not fail
