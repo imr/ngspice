imr/ngspice

commit fb63573b6b23e5247c3055aeca388858be35f577

Fix processing of lines like: "*# pre_some_command ...".

src/frontend/inp.c
@@ -742,6 +742,7 @@ inp_spsource(FILE *fp, bool comfile, char *filename, bool intfile)
         for (dd = deck->nextcard; dd; dd = ld->nextcard) {
             /* Ignore comment lines, but not lines begining with '*#',
                but remove them, if they are in a .control ... .endc section */
+
             s = skip_ws(dd->line);
             if ((*s == '*') && ((s != dd->line) || (s[1] != '#'))) {
                 if (commands) {
@@ -776,22 +777,31 @@ inp_spsource(FILE *fp, bool comfile, char *filename, bool intfile)
                 else
                     fprintf(cp_err, "Warning: misplaced .endc card\n");
             } else if (commands || prefix("*#", dd->line)) {
+                s = dd->line;
+
+                /* Special control lines outside of .control section? */
+
+                if (prefix("*#", s)) {
+                    s = skip_ws(s + 2);
+                    if (!*s)
+                        continue;
+                }
+
                 /* assemble all commands starting with pre_ after stripping
                  * pre_, to be executed before circuit parsing */
-                if (ciprefix("pre_", dd->line)) {
-                    s = copy(dd->line + 4);
-                    pre_controls = wl_cons(s, pre_controls);
-                }
-                /* assemble all other commands to be executed after circuit
-                 * parsing */
-                else {
-                    /* special control lines outside of .control section*/
-                    if (prefix("*#", dd->line)) {
-                        s = copy(dd->line + 2);
-                    /* all commands from within .control section */
+
+                if (ciprefix("pre_", s)) {
+                    s = s + 4;
+                    pre_controls = wl_cons(copy(s), pre_controls);
+                } else {
+                    /* Assemble all other commands to be executed
+                     * after circuit parsing */
+
+                    if (s == dd->line) {
+                        dd->line = NULL; /* Going to wl->wl_word. */
                     } else {
-                        s = dd->line;
-                        dd->line = NULL; /* SJB - prevent line_free() freeing the string (now pointed at by wl->wl_word) */
+                        s = copy(s);
+                        *dd->line = '*';
                     }
                     controls = wl_cons(s, controls);
                 }

commit 8d28b17ef8b018dee90c5cc4b89cc031ce9a8ad8

Fix Bug #733 -   "Pre-master-45 hangs forever on a circuit that works on older versions." That was an infinite loop when parsing a line with just "*#", introduced by commit fb63573b6b.  Also add some comments.

src/frontend/inp.c
@@ -738,7 +738,11 @@ inp_spsource(FILE *fp, bool comfile, char *filename, bool intfile)
     } /* end if (comfile) */
 
     else {  /* must be regular deck . . . . */
-        /* loop through deck and handle control cards */
+        /* Loop through deck and handle control cards.
+         * Pointer ld refers to the last card processed that has not
+         * been deleted.
+         */
+
         for (dd = deck->nextcard; dd; dd = ld->nextcard) {
             /* Ignore comment lines, but not lines begining with '*#',
                but remove them, if they are in a .control ... .endc section */
@@ -781,19 +785,17 @@ inp_spsource(FILE *fp, bool comfile, char *filename, bool intfile)
 
                 /* Special control lines outside of .control section? */
 
-                if (prefix("*#", s)) {
+                if (prefix("*#", s))
                     s = skip_ws(s + 2);
-                    if (!*s)
-                        continue;
-                }
-
-                /* assemble all commands starting with pre_ after stripping
-                 * pre_, to be executed before circuit parsing */
 
                 if (ciprefix("pre_", s)) {
+                    /* Assemble all commands starting with pre_ after stripping
+                     * pre_, to be executed before circuit parsing.
+                     */
+
                     s = s + 4;
                     pre_controls = wl_cons(copy(s), pre_controls);
-                } else {
+                } else if (*s) {
                     /* Assemble all other commands to be executed
                      * after circuit parsing */
 
@@ -832,10 +834,10 @@ inp_spsource(FILE *fp, bool comfile, char *filename, bool intfile)
                         ld->nextcard = dd->nextcard;
                         line_free(dd, FALSE);
                     } else {
-                        ld = dd;
+                        ld = dd; // Keep this card
                     }
                 } else {
-                    ld = dd;
+                    ld = dd;     // ... and this.
                 }
             }
         }  /* end for (dd = deck->nextcard . . . .  */