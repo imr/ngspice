684dad0f9dd917be446c218a258d042339eb8a18
Brian Taylor <lbwnet@comcast.net>
2025-02-16 16:09:42 -0800

    Fix memory leak of parm when there is a closing ).

--- src/xspice/mif/mifgetmod.c
+++ src/xspice/mif/mifgetmod.c
@@ -198,8 +198,10 @@ char *MIFgetMod(
                 num_pars = *device->numModelParms;
                 while (*line != '\0') {
                     INPgetTok(&line, &parm, 1);
-                    if (!parm || !*parm) // May be closing ')'.
+                    if (!parm || !*parm) { // May be closing ')'.
+                        FREE(parm);
                         break;
+                    }
                     for (j = 0; j < num_pars; j++) {
                         if (strcmp(parm, device->modelParms[j].keyword) == 0) {
                             err1 = NULL;
