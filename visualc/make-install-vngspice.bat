d3be53b1dcf31816b762064152f48920c408d583
dwarning <dwarning>
2025-03-02 08:26:54 +0100

    Remove 32 bit platform and make fftw3 library usage dependent form availability of the externaly components (*.h, *.lib and *.dll), if not available internal fft routines are used.

--- visualc/make-install-vngspice.bat
+++ visualc/make-install-vngspice.bat
@@ -1,41 +1,8 @@
 @echo off
 
 REM copy ngspice.exe, codemodels *.cm to C:\Spice or Spice64
-REM arguments to make-install-vngspiced:
-REM %1: path to ngspice.exe, %2, %3: fftw or 64 (64 bit)
-
-if "%2" == "64" goto b64
-if "%3" == "64" goto b64
-
-set dst=c:\Spice
-set cmsrc=.\codemodels\Win32\Release
-
-mkdir %dst%\bin
-mkdir %dst%\lib\ngspice
-
-copy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x86\Microsoft.VC140.OPENMP\vcomp140.dll" %dst%\bin\
-copy %cmsrc%\analog.cm %dst%\lib\ngspice\analog.cm
-copy %cmsrc%\digital.cm %dst%\lib\ngspice\digital.cm
-copy %cmsrc%\table.cm %dst%\lib\ngspice\table.cm
-copy %cmsrc%\xtraevt.cm %dst%\lib\ngspice\xtraevt.cm
-copy %cmsrc%\xtradev.cm %dst%\lib\ngspice\xtradev.cm
-copy %cmsrc%\spice2poly.cm %dst%\lib\ngspice\spice2poly.cm
-copy xspice\verilog\ivlng.dll %dst%\lib\ngspice\ivlng.dll
-copy xspice\verilog\shim.vpi %dst%\lib\ngspice\ivlng.vpi
-
-if "%2" == "fftw" goto copy2
-if "%3" == "fftw" goto copy2
-
-copy %1\ngspice.exe %dst%\bin\
-copy .\spinitr .\spinit
-goto end
-
-:copy2
-copy %1\ngspice.exe %dst%\bin\
-copy ..\..\fftw-3.3-dll32\libfftw3-3.dll %dst%\bin\
-goto end
-
-:b64
+REM arguments to make-install-vngspice:
+REM %1: path to ngspice.exe, %2: fftw
 
 set dst=c:\Spice64
 set cmsrc=.\codemodels\x64\Release
@@ -43,7 +10,7 @@ set cmsrc=.\codemodels\x64\Release
 mkdir %dst%\bin
 mkdir %dst%\lib\ngspice
 
-copy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x64\Microsoft.VC140.OPENMP\vcomp140.dll" %dst%\bin\
+copy "c:\Program Files\Microsoft Visual Studio\2022\Community\VC\Redist\MSVC\14.42.34433\debug_nonredist\x64\Microsoft.VC143.OpenMP.LLVM\libomp140.x86_64.dll" %dst%\bin\
 copy %cmsrc%\analog64.cm %dst%\lib\ngspice\analog.cm
 copy %cmsrc%\digital64.cm %dst%\lib\ngspice\digital.cm
 copy %cmsrc%\table64.cm %dst%\lib\ngspice\table.cm
@@ -54,7 +21,6 @@ copy xspice\verilog\ivlng.dll %dst%\lib\ngspice\ivlng.dll
 copy xspice\verilog\shim.vpi %dst%\lib\ngspice\ivlng.vpi
 
 if "%2" == "fftw" goto copy2-64
-if "%3" == "fftw" goto copy2-64
 
 copy %1\ngspice.exe %dst%\bin\
 copy .\spinitr64 .\spinit
