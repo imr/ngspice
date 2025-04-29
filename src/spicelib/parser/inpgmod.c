33f18b485ad59f515ba94b73fafebbaba84f4def
Holger Vogt <holger.vogt@uni-due.de>
2025-04-29 11:32:46 +0200

    Improve error messages when reading (and discarding) binned models, e.g. for Skywater PDK with up to 160 bins per model parameter set.

--- src/spicelib/parser/inpgmod.c
+++ src/spicelib/parser/inpgmod.c
@@ -286,6 +286,7 @@ INPgetModBin(CKTcircuit *ckt, char *name, INPmodel **model, INPtables *tab, char
 
     for (modtmp = modtab; modtmp; modtmp = modtmp->INPnextModel) {
 
+        /* exact: 1, with binning extension .[0-9]: 2*/
         if (model_name_match(name, modtmp->INPmodName) < 2)
             continue;
 
@@ -328,8 +329,8 @@ INPgetModBin(CKTcircuit *ckt, char *name, INPmodel **model, INPtables *tab, char
             *model = modtmp;
             return NULL;
         }
+        fprintf(stderr, "Warning: no model found for w=%.3e and l=%.3e\n", w, l);
     }
-
     return NULL;
 }
 
