30ab455b64c3caab8e836b15bafe5615db9496b8
Holger Vogt <holger.vogt@uni-due.de>
2025-06-06 11:56:57 +0200

    Enable real (from fft)or complex (from sp) frequency vetors in plot sp

--- src/frontend/com_measure2.c
+++ src/frontend/com_measure2.c
@@ -717,7 +717,10 @@ measure_at(
                 value = get_value(meas, d, i); //d->v_compdata[i].cx_real;
             else
                 value = d->v_realdata[i];
-            svalue = dScale->v_realdata[i];
+            if (dScale->v_compdata) // .sp, s-param.
+                svalue = dScale->v_compdata[i].cx_real;
+            else // fft
+                svalue = dScale->v_realdata[i];
         } else {
             value = d->v_realdata[i];
             svalue = dScale->v_realdata[i];
