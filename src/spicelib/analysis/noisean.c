imr/ngspice

commit d5ef8a366033604d349a3e5a4a3b5e9c8f69c0ea

Remove all entries connected to IPC, an outdated (>30 years) method to link ngspice to a now defunct controller (ATESSE).

src/spicelib/analysis/noisean.c
@@ -65,16 +65,9 @@ NOISEan(CKTcircuit* ckt, int restart)
     runDesc* plot = NULL;
 
 #ifdef XSPICE
-    /* gtri - add - wbk - 12/19/90 - Add IPC stuff and anal_init and anal_type */
-
-        /* Tell the beginPlot routine what mode we're in */
-    g_ipc.anal_type = IPC_ANAL_NOI;
-
     /* Tell the code models what mode we're in */
     g_mif_info.circuit.anal_type = MIF_DC;
     g_mif_info.circuit.anal_init = MIF_TRUE;
-
-    /* gtri - end - wbk */
 #endif
 
 #ifdef KLU
@@ -193,15 +186,15 @@ NOISEan(CKTcircuit* ckt, int restart)
 #endif
 
 #ifdef KLU
-        if (ckt->CKTmatrix->CKTkluMODE)
-        {
-            /* Conversion from Complex Matrix to Real Matrix */
-            for (i = 0 ; i < DEVmaxnum ; i++)
-                if (DEVices [i] && DEVices [i]->DEVbindCSCComplexToReal && ckt->CKThead [i])
-                    DEVices [i]->DEVbindCSCComplexToReal (ckt->CKThead [i], ckt) ;
+            if (ckt->CKTmatrix->CKTkluMODE)
+            {
+                /* Conversion from Complex Matrix to Real Matrix */
+                for (i = 0 ; i < DEVmaxnum ; i++)
+                    if (DEVices [i] && DEVices [i]->DEVbindCSCComplexToReal && ckt->CKThead [i])
+                        DEVices [i]->DEVbindCSCComplexToReal (ckt->CKThead [i], ckt) ;
 
-            ckt->CKTmatrix->SMPkluMatrix->KLUmatrixIsComplex = KLUmatrixReal ;
-        }
+                ckt->CKTmatrix->SMPkluMatrix->KLUmatrixIsComplex = KLUmatrixReal ;
+            }
 #endif
 
             /* If no event-driven instances, do what SPICE normally does */
@@ -220,42 +213,7 @@ NOISEan(CKTcircuit* ckt, int restart)
             else {
                 fprintf(stdout, "\n Linear circuit, option noopac given: no OP analysis\n");
             }
-
-#ifdef XSPICE
         } // end of no XSPICE event-driven instances
-
-        /* gtri - add - wbk - 12/19/90 - Add IPC stuff */
-
-            /* Send the operating point results for Mspice compatibility */
-        if (g_ipc.enabled)
-        {
-            /* Call CKTnames to get names of nodes/branches used by
-                BeginPlot */
-                /* Probably should free nameList after this block since
-                    called again... */
-            error = CKTnames(ckt, &numNames, &nameList);
-            if (error) return(error);
-
-            /* We have to do a beginPlot here since the data to return is
-             * different for the DCOP than it is for the AC analysis.
-             * Moreover the begin plot has not even been done yet at this
-             * point...
-             */
-            SPfrontEnd->OUTpBeginPlot(ckt, ckt->CKTcurJob,
-                ckt->CKTcurJob->JOBname,
-                NULL, IF_REAL,
-                numNames, nameList, IF_REAL,
-                &noiPlot);
-            txfree(nameList);
-
-            ipc_send_dcop_prefix();
-            CKTdump(ckt, 0.0, noiPlot);
-            ipc_send_dcop_suffix();
-
-            SPfrontEnd->OUTendPlot(noiPlot);
-        }
-        /* gtri - end - wbk */
-#endif
 
         /* Patch to noisean.c by Richard D. McRoberts. */
         ckt->CKTmode = (ckt->CKTmode & MODEUIC) | MODEDCOP | MODEINITSMSIG;

configure.ac
@@ -1428,7 +1428,6 @@ AC_CONFIG_FILES([Makefile
                  src/xspice/mif/Makefile
                  src/xspice/evt/Makefile
                  src/xspice/enh/Makefile
-                 src/xspice/ipc/Makefile
                  src/xspice/idn/Makefile
                  src/xspice/verilog/Makefile
                  src/xspice/vhdl/Makefile

src/Makefile.am
@@ -163,7 +163,6 @@ ngspice_LDADD += \
 	xspice/mif/libmifxsp.la \
 	xspice/evt/libevtxsp.la \
 	xspice/enh/libenhxsp.la \
-	xspice/ipc/libipcxsp.la \
 	xspice/idn/libidnxsp.la 
 endif
 ngspice_LDADD += $(XSPICEDLLIBS)
@@ -459,7 +458,6 @@ if XSPICE_WANTED
 libspice_la_LIBADD += \
 	xspice/evt/libevtxsp.la \
 	xspice/enh/libenhxsp.la \
-	xspice/ipc/libipcxsp.la \
 	xspice/idn/libidnxsp.la 
 endif
 libspice_la_LIBADD += $(XSPICEDLLIBS)
@@ -586,7 +584,6 @@ if XSPICE_WANTED
 libngspice_la_LIBADD += \
 	xspice/evt/libevtxsp.la \
 	xspice/enh/libenhxsp.la \
-	xspice/ipc/libipcxsp.la \
 	xspice/idn/libidnxsp.la 
 endif
 libngspice_la_LIBADD += $(XSPICEDLLIBS)

src/frontend/inp.c
@@ -244,13 +244,6 @@ inp_list(FILE *file, struct card *deck, struct card *extras, int type)
     bool useout = (file == cp_out);
     int i = 1;
 
-    /* gtri - wbk - 03/07/91 - Don't use 'more' type output if ipc enabled */
-#ifdef XSPICE
-    if (g_ipc.enabled)
-        useout = FALSE;
-#endif
-    /* gtri - end - 03/07/91 */
-
     if (useout) {
         out_init();
         file = cp_more;
@@ -1474,10 +1467,6 @@ inp_dodeck(
 
         if (dd->error) {
             char *p, *q;
-#ifdef XSPICE
-            /* add setting of ipc syntax error flag */
-            g_ipc.syntax_error = IPC_TRUE;
-#endif
             p = dd->error;
             fflush(stdout);
             do {

src/frontend/inpcom.c
@@ -1355,53 +1355,24 @@ static struct inp_read_t inp_read(FILE* fp, int call_depth, const char* dir_name
         else {
 
 #ifdef XSPICE
-            /* gtri - modify - 12/12/90 - wbk - read from mailbox if ipc
-             * enabled */
 
-             /* If IPC is not enabled, do equivalent of what SPICE did before
-              */
-            if (!g_ipc.enabled) {
-                if (call_depth == 0 && line_count == 0) {
-                    line_count++;
-                    if (fgets(big_buff, 5000, fp))
-                        buffer = copy(big_buff);
-                }
-                else {
-                    buffer = readline(fp);
-                    if (!buffer)
-                        break;
-                }
+            if (call_depth == 0 && line_count == 0) {
+                line_count++;
+                if (fgets(big_buff, 5000, fp))
+                    buffer = copy(big_buff);
             }
             else {
-                /* else, get the line from the ipc channel. */
-                /* We assume that newlines are not sent by the client */
-                /* so we add them here */
-                char ipc_buffer[1025]; /* Had better be big enough */
-                int ipc_len;
-                Ipc_Status_t ipc_status =
-                    ipc_get_line(ipc_buffer, &ipc_len, IPC_WAIT);
-                if (ipc_status == IPC_STATUS_END_OF_DECK) {
-                    buffer = NULL;
+                buffer = readline(fp);
+                if (!buffer)
                     break;
-                }
-                else if (ipc_status == IPC_STATUS_OK) {
-                    buffer = TMALLOC(char, strlen(ipc_buffer) + 3);
-                    strcpy(buffer, ipc_buffer);
-                    strcat(buffer, "\n");
-                }
-                else { /* No good way to report this so just die */
-                    fprintf(stderr, "Error: IPC status not o.k.\n");
-                    controlled_exit(EXIT_FAILURE);
-                }
             }
 
-            /* gtri - end - 12/12/90 */
 #else
 
             buffer = readline(fp);
             if (!buffer) {
                 break;
-        }
+            }
 
 #endif
         }

src/frontend/resource.c
@@ -213,14 +213,6 @@ printres(char *name)
             called = TRUE;
         }
 
-#ifdef XSPICE
-        /* gtri - add - 12/12/90 - wbk - record cpu time used for ipc */
-        g_ipc.cpu_time = (double) last_msec;
-        g_ipc.cpu_time /= 1000.0;
-        g_ipc.cpu_time += (double) last_sec;
-        /* gtri - end - 12/12/90 */
-#endif
-
         yy = TRUE;
 #else
         if (!name || eq(name, "totalcputime"))

src/frontend/runcoms.c
@@ -323,14 +323,6 @@ static int dosim(
         if (if_sens_run(ft_curckt->ci_ckt, ww, ft_curckt->ci_symtab) == 1) {
             /* The circuit was interrupted somewhere. */
             fprintf(cp_err, "%s simulation interrupted\n", what);
-#ifdef XSPICE
-            /* gtri - add - 12/12/90 - wbk - record error and return errchk */
-            g_ipc.run_error = IPC_TRUE;
-            if (g_ipc.enabled) {
-                ipc_send_errchk();
-            }
-            /* gtri - end - 12/12/90 */
-#endif
         }
         else {
             ft_curckt->ci_inprogress = FALSE;
@@ -342,14 +334,6 @@ static int dosim(
         if (err == 1) {
             /* The circuit was interrupted somewhere. */
             fprintf(cp_err, "%s simulation interrupted\n", what);
-#ifdef XSPICE
-            /* record error and return errchk */
-            g_ipc.run_error = IPC_TRUE;
-            if (g_ipc.enabled) {
-                ipc_send_errchk();
-            }
-            /* gtri - end - 12/12/90 */
-#endif
             err = 0;
         }
         else if (err == 2) {

src/spicelib/analysis/acan.c
@@ -62,16 +62,9 @@ ACan(CKTcircuit* ckt, int restart)
 
 
 #ifdef XSPICE
-    /* gtri - add - wbk - 12/19/90 - Add IPC stuff and anal_init and anal_type */
-
-        /* Tell the beginPlot routine what mode we're in */
-    g_ipc.anal_type = IPC_ANAL_AC;
-
     /* Tell the code models what mode we're in */
     g_mif_info.circuit.anal_type = MIF_DC;
     g_mif_info.circuit.anal_init = MIF_TRUE;
-
-    /* gtri - end - wbk */
 #endif
 
     /* start at beginning */
@@ -151,40 +144,6 @@ ACan(CKTcircuit* ckt, int restart)
             }
             else
                 fprintf(stdout, "\n Linear circuit, option noopac given: no OP analysis\n");
-
-#ifdef XSPICE
-        /* gtri - add - wbk - 12/19/90 - Add IPC stuff */
-
-            /* Send the operating point results for Mspice compatibility */
-        if (g_ipc.enabled)
-        {
-            /* Call CKTnames to get names of nodes/branches used by
-                BeginPlot */
-                /* Probably should free nameList after this block since
-                    called again... */
-            error = CKTnames(ckt, &numNames, &nameList);
-            if (error) return(error);
-
-            /* We have to do a beginPlot here since the data to return is
-             * different for the DCOP than it is for the AC analysis.
-             * Moreover the begin plot has not even been done yet at this
-             * point...
-             */
-            SPfrontEnd->OUTpBeginPlot(ckt, ckt->CKTcurJob,
-                ckt->CKTcurJob->JOBname,
-                NULL, IF_REAL,
-                numNames, nameList, IF_REAL,
-                &acPlot);
-            txfree(nameList);
-
-            ipc_send_dcop_prefix();
-            CKTdump(ckt, 0.0, acPlot);
-            ipc_send_dcop_suffix();
-
-            SPfrontEnd->OUTendPlot(acPlot);
-        }
-        /* gtri - end - wbk */
-#endif
 
         ckt->CKTmode = (ckt->CKTmode & MODEUIC) | MODEDCOP | MODEINITSMSIG;
         error = CKTload(ckt);
@@ -376,21 +335,8 @@ ACan(CKTcircuit* ckt, int restart)
         }
 #endif
 
-#ifdef XSPICE
-        /* gtri - modify - wbk - 12/19/90 - Send IPC stuff */
-
-        if (g_ipc.enabled)
-            ipc_send_data_prefix(freq);
-
         error = CKTacDump(ckt, freq, acPlot);
 
-        if (g_ipc.enabled)
-            ipc_send_data_suffix();
-
-        /* gtri - modify - wbk - 12/19/90 - Send IPC stuff */
-#else
-        error = CKTacDump(ckt, freq, acPlot);
-#endif	
         if (error) {
             UPDATE_STATS(DOING_AC);
             return(error);

commit f8036c5363f48101a7194d11c3aaec10fca9f936

fix bug no 819, which has been introduced by d5ef8a366 ("Remove all entries connected to IPC, an outdated (>30 years) method to link ngspice to a now defunct controller (ATESSE).", 2025-10-21)

src/spicelib/analysis/noisean.c
@@ -182,9 +182,8 @@ NOISEan(CKTcircuit* ckt, int restart)
             EVTop_save(ckt, MIF_TRUE, 0.0);
         }
         else
-        { // start of no XSPICE event-driven instances
 #endif
-
+        { // start of no XSPICE event-driven instances
 #ifdef KLU
             if (ckt->CKTmatrix->CKTkluMODE)
             {